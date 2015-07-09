05f2243ce466b135c2f5aec4863e2d191ca44718
tdphillips <troy@apps.directemployers.org>
2015-07-09 16:22:28 -0400

    Comments <PD-1520><PD-1521><PD-1522>

--- postajob/management/commands/test_posting.py
+++ postajob/management/commands/test_posting.py
@@ -23,6 +23,16 @@ class Command(NoArgsCommand):
 
 
 def make_user(address, admin=False):
+    """
+    Creates a normal user or superuser using the provided address
+
+    Inputs:
+    :address: User's email address
+    :admin: Boolean, is this user an admin
+
+    Outputs:
+    :user: Generated user
+    """
     if admin:
         method = 'create_superuser'
     else:
@@ -53,33 +63,52 @@ class JobPostingTests(TestCase):
 
     @classmethod
     def setup_objects(cls):
+        """
+        This is a big ugly method that sets up objects needed for postajob
+        testing.
+
+        Since we're modifying an active database, we need to follow an old
+        dumpster diving rule and leave it cleaner than we found it. As such,
+        every addition is being added to a list so that they can be reversed.
+        This is a little verbose - these additions are being appended to the
+        list immediately so that we can adequately revert if something fails.
+        """
+        #Users:
+        # These are all superusers at the moment as the admin page is being
+        # used to log in. It isn't guaranteed that a given microsite has a
+        # login page set up.
+
+        # admin: Main user for testing
         cls.admin = make_user(address='paj_admin@my.jobs', admin=True)
-        # I'm appending after every db write rather than at the very end
-        # so we can roll back all previous successes if any transaction fails.
         cls.CREATION_ORDER.append(cls.admin)
+        # admin_2: Secondary company user for a different company
         cls.admin_2 = make_user(address='paj_admin_2@my.jobs', admin=True)
         cls.CREATION_ORDER.append(cls.admin_2)
+        # user: User unaffiliated to any company
         cls.user = make_user(address='paj_user@my.jobs', admin=True)
         cls.CREATION_ORDER.append(cls.user)
 
+        # Companies:
         cls.admin_company = Company.objects.create(
             name='Postajob Selenium Company',
             company_slug='postajob-selenium-company',
             member=True, product_access=True, posting_access=True)
         cls.CREATION_ORDER.append(cls.admin_company)
-        cls.admin_company_user = CompanyUser.objects.create(
-            company=cls.admin_company, user=cls.admin)
-        cls.CREATION_ORDER.append(cls.admin_company_user)
-
         cls.admin_company_2 = Company.objects.create(
             name='Postajob Selenium Company 2',
             company_slug='postajob-selenium-company-2',
             member=True, product_access=True, posting_access=True)
         cls.CREATION_ORDER.append(cls.admin_company_2)
+
+        # Company Users:
+        cls.admin_company_user = CompanyUser.objects.create(
+            company=cls.admin_company, user=cls.admin)
+        cls.CREATION_ORDER.append(cls.admin_company_user)
         cls.admin_company_user_2 = CompanyUser.objects.create(
             company=cls.admin_company_2, user=cls.admin_2)
         cls.CREATION_ORDER.append(cls.admin_company_user_2)
 
+        # Seo Sites:
         cls.seo_site = SeoSite.objects.create(
             domain='selenium.jobs', name='Selenium Jobs',
             canonical_company=cls.admin_company)
@@ -89,6 +118,7 @@ class JobPostingTests(TestCase):
             canonical_company=cls.admin_company_2)
         cls.CREATION_ORDER.append(cls.seo_site_2)
 
+        # Site Packages:
         cls.site_package = SitePackage.objects.create(
             owner=cls.admin_company)
         cls.site_package.sites.add(cls.seo_site)
@@ -100,6 +130,12 @@ class JobPostingTests(TestCase):
 
     @classmethod
     def login(cls, user):
+        """
+        Logs the provided user in using our web driver.
+
+        Inputs:
+        :user: User being logged in
+        """
         cls.get('/admin/')
         cls.browser.find_element_by_id('id_username').send_keys(user.email)
         cls.browser.find_element_by_id('id_password').send_keys(
@@ -108,6 +144,9 @@ class JobPostingTests(TestCase):
 
     @classmethod
     def logout(cls):
+        """
+        Logs out whoever is logged in, if anyone.
+        """
         cls.get('/admin/')
         try:
             element = cls.browser.find_element_by_xpath(
@@ -117,9 +156,16 @@ class JobPostingTests(TestCase):
         else:
             element.click()
 
-
     @classmethod
     def post(cls, path='/', data=None, domain=None):
+        """
+        Shortcut to cls.browser.post(...) with additional options.
+
+        Inputs:
+        :path: Path being hit
+        :data: A dictionary of values to be posted, if any
+        :domain: String used for domain overrides
+        """
         data = data or {}
         requested_url = 'http://{domain}{port}{path}'.format(
             domain=cls.test_url, port=cls.test_port, path=path)
@@ -129,6 +175,13 @@ class JobPostingTests(TestCase):
 
     @classmethod
     def get(cls, path='/', domain=None):
+        """
+        Shortcut to cls.browser.get(...) with additional options.
+
+        Inputs:
+        :path: Path being hit
+        :domain: String used for domain overrides
+        """
         requested_url = 'http://{domain}{port}{path}'.format(
             domain=cls.test_url, port=cls.test_port, path=path)
         if domain:
@@ -137,6 +190,9 @@ class JobPostingTests(TestCase):
 
     @classmethod
     def setUpClass(cls):
+        """
+        Sets up the test environment, overriding settings and modifying the db.
+        """
         environment = os.environ.get('SETTINGS', '').lower()
         if environment == 'qc':
             print 'Running test_posting with QC settings'
@@ -154,6 +210,8 @@ class JobPostingTests(TestCase):
             assert getattr(settings, 'ENVIRONMENT') != 'Production', \
                 'Running test_posting with production settings is unsupported'
             print 'Running test_posting with settings.py'
+            # Assuming local; I have to pick a port and runserver defaults to
+            # 8000, so...
             cls.test_port = ':8000'
         cls.browser = webdriver.PhantomJS()
         super(JobPostingTests, cls).setUpClass()
@@ -163,6 +221,9 @@ class JobPostingTests(TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Deletes all objects created during setup.
+        """
         cls.browser.quit()
         with patch_settings(**cls.OVERRIDES):
             for obj in cls.CREATION_ORDER[::-1]:
@@ -170,20 +231,26 @@ class JobPostingTests(TestCase):
         super(JobPostingTests, cls).tearDownClass()
 
     def test_show_job_admin(self):
+        """
+        Ensures that the main postajob admin is functional.
+
+        A company user for the site owner should be able to see all options.
+        A company user for a third party or a non-company-user should not.
+        """
         with patch_settings(**self.OVERRIDES):
             for user, accessible in [(self.admin, True), (self.admin_2, False),
                                      (self.user, False)]:
-                print user.email
                 self.login(user)
                 self.get(reverse('purchasedmicrosite_admin_overview'),
                          domain=self.seo_site.domain)
-                self.browser.get_screenshot_as_file('file.png')
                 for selector, expected in [('product-listing', 'Product Listing'),
                                            ('our-postings', 'Posted Jobs'),
                                            ('posting-admin', 'Partner Microsite')]:
                     try:
                         element = self.browser.find_element_by_id(selector)
                     except NoSuchElementException:
+                        # If the user is not a company user for the owner, this
+                        # is expected; if not, we should reraise and fail.
                         if accessible:
                             raise
                     else:
