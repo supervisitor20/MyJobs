19c6ca91e6fd64a2f32bf7586ba77a2e811c4585
Matt Lauber <github@mklauber.com>
2015-07-13 08:00:03 -0400

    Copy Paste issue.

--- seo/management/commands/requeue_failures.py
+++ seo/management/commands/requeue_failures.py
@@ -5,7 +5,7 @@ from tasks import task_etl_to_solr
 
 class Command(NoArgsCommand):
     args = 'None'
-    help = 'Checks for the existence of member logos on the content server'
+    help = 'REqueues tasks that failed in the last eight hours.'
 
     def handle_noargs(self, **options):
         eight_hours_ago = datetime.now() - timedelta(hours=8)
