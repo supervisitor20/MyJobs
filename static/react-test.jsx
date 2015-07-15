f14c0dc2890a007a91b871286d32b58f77989127
David Galitsky <galitskyd@gmail.com>
2015-07-15 14:21:01 -0400

    removed unnecessary data

--- static/react-test.jsx
+++ static/react-test.jsx
@@ -1,8 +1,3 @@
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var CommentBox = React.createClass({
   getInitialState: function() {
     return {data: []};
