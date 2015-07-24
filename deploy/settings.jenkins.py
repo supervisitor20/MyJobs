a72ba634b9eb31fbda9dff3623246d62dea79b91
Matt Lauber <github@mklauber.com>
2015-07-24 09:43:44 -0400

    Updated settings files.

--- deploy/settings.jenkins.py
+++ deploy/settings.jenkins.py
@@ -11,25 +11,25 @@ DATABASES = {
     'default': {
         'NAME': 'redirect',
         'ENGINE': 'django.db.backends.mysql',
-        'USER': 'de_dbuser',
-        'PASSWORD': PROD_DB_PASSWD,
-        'HOST': 'db-redirectstaging.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
+        'USER': 'root',
+        'PASSWORD': 'P@ssW0rd',
+        'HOST': '',
         'PORT': '3306',
     },
     'qc-redirect': {
         'NAME': 'qcredirect',
         'ENGINE': 'django.db.backends.mysql',
-        'USER': 'de_dbuser',
-        'PASSWORD': PROD_DB_PASSWD,
-        'HOST': 'db-redirectstaging.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
+        'USER': 'root',
+        'PASSWORD': 'P@ssW0rd',
+        'HOST': '',
         'PORT': '3306',
     },
     'archive': {
         'NAME': 'qcredirect',
         'ENGINE': 'django.db.backends.mysql',
-        'USER': 'de_dbuser',
-        'PASSWORD': PROD_DB_PASSWD,
-        'HOST': 'db-redirectstaging.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
+        'USER': 'root',
+        'PASSWORD': 'P@ssW0rd',
+        'HOST': '',
         'PORT': '3306',
     },
 }
@@ -73,3 +73,29 @@ HAYSTACK_CONNECTIONS = {
 TEMPLATE_CONTEXT_PROCESSORS += (
     'mymessages.context_processors.message_lists',
 )
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s "
+                      "[%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'WARN',
+        }
+    }
+}
