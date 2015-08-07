f67a244213de7facf50dcbd9eeed9c1cc9fe9c5f
tdphillips <troy@apps.directemployers.org>
2015-08-07 10:42:08 -0400

    Oops

--- default_settings.py
+++ default_settings.py
@@ -77,11 +77,11 @@ TEMPLATE_DIRS = (
 )
 
 TEMPLATE_LOADERS = (
-    #('django.template.loaders.cached.Loader', (
+    ('django.template.loaders.cached.Loader', (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
         'django.template.loaders.eggs.Loader',
-    #)),
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
