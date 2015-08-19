5cdf2654b4c01368f7556b51f16faf3f8fffc971
David Galitsky <galitskyd@gmail.com>
2015-08-19 09:56:22 -0400

    changed format for states_with_sites

--- universal/states.py
+++ universal/states.py
@@ -1,7 +1,6 @@
 import json
 
 from os import path
-from bidict import bidict
 
 from django.conf import settings
 
@@ -21,62 +20,62 @@ synonyms = dict(
     [(key.lower(), [key, value]) for key, value in get_state_map().items()] +
     [(value.lower(), [key, value]) for key, value in get_state_map().items()])
 
-states_with_sites = bidict({
-    "Alabama": "alabama.jobs",
-    "Alaska": "alaska.jobs",
-    "Arizona": "arizona.jobs",
-    "Arkansas": "arkansas.jobs",
-    "California": "www.california.jobs",
-    "Colorado": "colorado.jobs",
-    "Connecticut": "connecticut.jobs",
-    "Delaware": "delaware.jobs",
-    "Florida": "florida.jobs",
-    "Georgia": "georgia.jobs",
-    "Hawaii": "hawaii.jobs",
-    "Idaho": "idaho.jobs",
-    "Illinois": "illinois.jobs",
-    "Indiana": "indiana.jobs",
-    "Iowa": "iowa.jobs",
-    "Kansas": "kansas.jobs",
-    "Kentucky": "kentucky.jobs",
-    "Louisiana": "louisiana.jobs",
-    "Maine": "maine.jobs",
-    "Maryland": "maryland.jobs",
-    "Massachusetts": "massachusetts.jobs",
-    "Michigan": "michigan.jobs",
-    "Minnesota": "minnesota.jobs",
-    "Mississippi": "mississippi.jobs",
-    "Missouri": "missouri.jobs",
-    "Montana": "montana.jobs",
-    "Nebraska": "nebraska.jobs",
-    "Nevada": "nevada.jobs",
-    "New Hampshire": "newhampshire.jobs",
-    "New Jersey": "newjersey.jobs",
-    "New Mexico": "newmexico.jobs",
-    "New York": "newyork.jobs",
-    "North Carolina": "northcarolina.jobs",
-    "North Dakota": "northdakota.jobs",
-    "Ohio": "ohio.jobs",
-    "Oklahoma": "oklahoma.jobs",
-    "Oregon": "oregon.jobs",
-    "Pennsylvania": "pennsylvania.jobs",
-    "Rhode Island": "rhodeisland.jobs",
-    "South Carolina": "southcarolina.jobs",
-    "South Dakota": "southdakota.jobs",
-    "Tennessee": "tennessee.jobs",
-    "Texas": "texas.jobs",
-    "Utah": "utah.jobs",
-    "Vermont": "vermont.jobs",
-    "Virginia": "virginia.jobs",
-    "Washington": "washington.jobs",
-    "West Virginia": "westvirginia.jobs",
-    "Wisconsin": "wisconsin.jobs",
-    "Wyoming": "wyoming.jobs"
-})
+states_with_sites = [
+    {"state": "Alabama",        "url": "alabama.jobs"},
+    {"state": "Alaska",         "url": "alaska.jobs"},
+    {"state": "Arizona",        "url": "arizona.jobs"},
+    {"state": "Arkansas",       "url": "arkansas.jobs"},
+    {"state": "California",     "url": "california.jobs"},
+    {"state": "Colorado",       "url": "colorado.jobs"},
+    {"state": "Connecticut",    "url": "connecticut.jobs"},
+    {"state": "Delaware",       "url": "delaware.jobs"},
+    {"state": "Florida",        "url": "florida.jobs"},
+    {"state": "Georgia",        "url": "georgia.jobs"},
+    {"state": "Hawaii",         "url": "hawaii.jobs"},
+    {"state": "Idaho",          "url": "idaho.jobs"},
+    {"state": "Illinois",       "url": "illinois.jobs"},
+    {"state": "Indiana",        "url": "indiana.jobs"},
+    {"state": "Iowa",           "url": "iowa.jobs"},
+    {"state": "Kansas",         "url": "kansas.jobs"},
+    {"state": "Kentucky",       "url": "kentucky.jobs"},
+    {"state": "Louisiana",      "url": "louisiana.jobs"},
+    {"state": "Maine",          "url": "maine.jobs"},
+    {"state": "Maryland",       "url": "maryland.jobs"},
+    {"state": "Massachusetts",  "url": "massachusetts.jobs"},
+    {"state": "Michigan",       "url": "michigan.jobs"},
+    {"state": "Minnesota",      "url": "minnesota.jobs"},
+    {"state": "Mississippi",    "url": "mississippi.jobs"},
+    {"state": "Missouri",       "url": "missouri.jobs"},
+    {"state": "Montana",        "url": "montana.jobs"},
+    {"state": "Nebraska",       "url": "nebraska.jobs"},
+    {"state": "Nevada",         "url": "nevada.jobs"},
+    {"state": "New Hampshire",  "url": "newhampshire.jobs"},
+    {"state": "New Jersey",     "url": "newjersey.jobs"},
+    {"state": "New Mexico",     "url": "newmexico.jobs"},
+    {"state": "New York",       "url": "newyork.jobs"},
+    {"state": "North Carolina", "url": "northcarolina.jobs"},
+    {"state": "North Dakota",   "url": "northdakota.jobs"},
+    {"state": "Ohio",           "url": "ohio.jobs"},
+    {"state": "Oklahoma",       "url": "oklahoma.jobs"},
+    {"state": "Oregon",         "url": "oregon.jobs"},
+    {"state": "Pennsylvania",   "url": "pennsylvania.jobs"},
+    {"state": "Rhode Island",   "url": "rhodeisland.jobs"},
+    {"state": "South Carolina", "url": "southcarolina.jobs"},
+    {"state": "South Dakota",   "url": "southdakota.jobs"},
+    {"state": "Tennessee",      "url": "tennessee.jobs"},
+    {"state": "Texas",          "url": "texas.jobs"},
+    {"state": "Utah",           "url": "utah.jobs"},
+    {"state": "Vermont",        "url": "vermont.jobs"},
+    {"state": "Virginia",       "url": "virginia.jobs"},
+    {"state": "Washington",     "url": "washington.jobs"},
+    {"state": "West Virginia",  "url": "westvirginia.jobs"},
+    {"state": "Wisconsin",      "url": "wisconsin.jobs"},
+    {"state": "Wyoming",        "url": "wyoming.jobs"}
+]
 
-other_locations_with_sites = bidict({
-    "District of Columbia": "districtofcolumbia.jobs",
-    "Guam": "guam.jobs",
-    "Puerto Rico": "puertorico.jobs",
-    "Virgin Islands": "usvirginislands.jobs"
-})
+other_locations_with_sites = [
+    {"state": "District Of Columbia",   "url": "districtofcolumbia.jobs"},
+    {"state": "Guam",                   "url": "guam.jobs"},
+    {"state": "Puerto Rico",            "url": "puertorico.jobs"},
+    {"state": "Virgin Islands",         "url": "usvirginislands.jobs"}
+]
