7128f7ab10f6d93f059748c3d4ea9f671901ca4f
aspidites <emarshall85@gmail.com>
2015-08-28 11:04:37 -0400

    Removed extra character from view urls and started writing tests.

--- mypartners/tests/test_forms.py
+++ mypartners/tests/test_forms.py
@@ -1,12 +1,11 @@
 from django.core.urlresolvers import reverse
 from myjobs.tests.factories import UserFactory
 from mypartners.forms import ContactForm, ContactRecordForm
-from mypartners.models import Contact, Location
-from mypartners.tests.factories import ContactFactory
+from mypartners.models import Contact, Location, ContactRecord
+from mypartners.tests.factories import ContactFactory, ContactRecordFactory
 from mypartners.tests.test_views import MyPartnersTestCase
 from mysearches.tests.factories import PartnerSavedSearchFactory
-from mysearches.forms import PartnerSavedSearchForm
-
+from mysearches.forms import PartnerSavedSearchForm 
 
 class ContactFormTests(MyPartnersTestCase):
     def setUp(self):
@@ -87,12 +86,98 @@ class PartnerSavedSearchFormTests(MyPartnersTestCase):
 
 
 class ContactRecordFormTests(MyPartnersTestCase):
+    def setUp(self):
+        super(ContactRecordFormTests, self).setUp()
+
+        self.contact_record = ContactRecordFactory(
+            contact=self.contact, partner=self.partner)
+
+        # The contact record form has a lot of required fields.
+        self.data = {
+            'contact_type': self.contact_record.contact_type,
+            'contact': self.contact_record.contact.pk,
+            'contact_email': self.contact_record.contact_email,
+            'length_0': "00",
+            'length_1': "30",
+            'date_time_0': "Aug",
+            'date_time_1': "24",
+            'date_time_2': "2015",
+            'date_time_3': "03",
+            'date_time_4': "10",
+            'date_time_5': "PM",
+            'notes': self.contact_record.notes,
+            'partner': self.contact_record.partner.pk,
+            'company': self.contact_record.partner.owner
+        }
+        self.form = ContactRecordForm(
+            instance=self.contact_record, partner=self.partner, data=self.data)
+
+        # add a few contact records so that we have some to select from
+        ContactRecordFactory.create_batch(3, partner=self.partner,
+                                          contact=self.contact,
+                                          contact_type='phone')
+
+        self.client.path = reverse(
+            "partner_edit_record") + "?partner=%s" % self.partner.pk
+
+
     def test_archived_contacts_not_shown(self):
         """Test that archived contacts aren't selectable."""
 
-        form = ContactRecordForm(partner=self.partner)
-        self.assertIn(self.contact, form.fields['contact'].queryset)
+        self.assertIn(self.contact, self.form.fields['contact'].queryset)
 
         self.contact.delete()
         form = ContactRecordForm(partner=self.partner)
         self.assertNotIn(self.contact, form.fields['contact'].queryset)
+
+    def test_that_creating_a_record_redirects_properly(self):
+        """
+        Tests that when you create a record, you are redirected to the view
+        which contains that record.
+        """
+        pass
+
+
+    def test_changing_contact_type_redirects_correctly(self):
+        """
+        Test that when saving a contact record, you are redirected to the view
+        which contains that record, even if you changed the contact type.
+        """
+
+        # the contact record we are interested in modifying and returning to
+        contact_record = ContactRecord.objects.all()[2]
+        # sanity check to make sure notes aren't modified yet
+        self.assertEqual(contact_record.notes, "Some notes go here.")
+        
+        self.data.update({
+            "contact_type": "phone",
+            "contact_phone": "555-555-5555",
+            "notes": "some test notes"
+        })
+        response = self.client.post(path=self.client.path + '&page=2',
+                                    data=self.data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn("some test notes", response.content)
+
+    def test_that_backdating_a_record_redirects_correctly(self):
+        """
+        Test that when saving a contact record, you are redirected to the view
+        which contains that record, even if back-date the record.
+        """
+
+        # the contact record we are interested in modifying and returning to
+        contact_record = ContactRecord.objects.all()[1]
+        # sanity check to make sure notes aren't modified yet
+        self.assertEqual(contact_record.notes, "Some notes go here.")
+        
+        self.data.update({
+            'date_time_2': "2012",
+            "notes": "some test notes"
+        })
+        response = self.client.post(path=self.client.path + '&page=2',
+                                    data=self.data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn("some test notes", response.content)
+
