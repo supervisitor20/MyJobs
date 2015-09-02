a2e73f98fdf7c7ba30c3ff28d0a013b5c9d0951b
aspidites <emarshall85@gmail.com>
2015-09-02 10:22:15 -0400

    PEP-8, stoppped using exceptional code.

--- seo/context_processors.py
+++ seo/context_processors.py
@@ -3,4 +3,4 @@ from seo.cache import get_site_config, get_domain_parent
 
 def site_config_context(request):
     return {'site_config': get_site_config(request),
-            'domain_parent':get_domain_parent(request)}
+            'domain_parent': get_domain_parent(request)}
