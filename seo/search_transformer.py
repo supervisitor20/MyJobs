70567ee5f756dfc6bfd5e027b2a34f7e59b875fd
Darrin Thompson <dthompson@apps.directemployers.org>
2015-09-08 13:53:32 -0400

    Add search transformer

---
+++ seo/search_transformer.py
@@ -0,0 +1,591 @@
+import re
+from sets import Set
+import itertools
+from functools import wraps
+import logging
+
+logger = logging.getLogger(__name__)
+
+# This is a basic compiler which transforms input search queries and
+# some context into queries ready for Solr.
+#
+# The compiler consists of:
+#
+# * A peekable token stream mechanism.
+#
+# * Tokenizer
+#       A Python generator that is responsible only for breaking the
+#       input into individual tokens.
+#
+# * Recursive descent parser
+#       A basic recursive descent parser that gets the input operator
+#       precendence right.
+#
+#       Includes a defensive mechanism to keep malicious input from
+#       causing the parser to give up after too many steps.
+#
+# * Optimizer
+#       Handles special cases.
+#
+# MAINTENANCE ADVICE:
+#
+# Something complex like this can grow unmaintainable even with the
+# unit test battery.
+#
+# * Make changes as close to the output as possible.
+#
+# * [BAD] Changes to the tokenizer affect everything downstream.
+#       It is tempting, since there are regex's here, to just tweak
+#       those. Little changes here can have hard to predict ripple
+#       effects. The tokenizer needs to be as simple as we can get
+#       away with.
+#
+# * [BAD] The parser's behavior is surprisingly senstive to tiny
+#       changes. Smaller is better her. Only change this if
+#       there is no other way.
+#
+# * [GOOD] Changes/additions to the optimizer patterns affect only
+#       other optimizer patterns. This is where we try to do as much
+#       work as we can.
+
+
+# Token Stream --------------------------------
+
+# For a recursive descent parser we need a stream of tokens where we
+# can peek ahead in the element stream without consuming the element.
+#
+# http://stackoverflow.com/questions/2425270/how-to-look-ahead-one-element-in-a-python-generator
+class Peekable(object):
+    """Wrap a generator so that we can peek at the first element.
+
+    Instead of requiring the user to handle StopIteration, return
+    a last element instead.
+
+    The last element is only returned by next. Iterating with for
+    will behave the usual python way.
+
+    This arrangement seems to be the most convenient for implementing
+    a parser.
+    """
+
+    def __init__(self, gen, last):
+        self.gen, = itertools.tee(gen, 1)
+        self.last = last
+
+    def __iter__(self):
+        return self.gen
+
+    def next(self):
+        try:
+            return iter(self).next()
+        except StopIteration:
+            return self.last
+
+    def peek(self):
+        try:
+            return self.gen.__copy__().next()
+        except StopIteration:
+            return self.last
+
+
+def peekable(last):
+    """Decorator for generators whose elements need to be peekable."""
+
+    def make_peekable(fn):
+        @wraps(fn)
+        def peekable_fn(*args):
+            return Peekable(fn(*args), last)
+        return peekable_fn
+    return make_peekable
+
+# Tokenizer -------------------------------
+
+and_ops = Set(["and", "&"])
+or_ops = Set(["or", ",", "|"])
+not_ops = Set(["not", "-", "!"])
+
+
+class Token(object):
+    """Represents a token found by the tokenizer."""
+
+    def __init__(self, token_type, token, flags=[]):
+        self.token_type = token_type
+        self.token = token
+        self.flags = flags
+
+    def is_or(self):
+        return self.token_type == 'op' and self.token in or_ops
+
+    def is_and(self):
+        return self.token_type == 'op' and self.token in and_ops
+
+    def is_not(self):
+        return self.token_type == 'op' and self.token in not_ops
+
+    def is_term(self):
+        return self.token_type == 'term'
+
+    def is_andable(self):
+        return (self.is_and() or
+                self.is_not() or
+                self.is_term())
+
+    def is_eof(self):
+        return self.token_type == 'eof'
+
+    def is_quote(self):
+        return self.token_type == 'quote'
+
+    def is_openparen(self):
+        return self.token_type == 'openp'
+
+    def is_closeparen(self):
+        return self.token_type == 'closep'
+
+    def __repr__(self):
+        return "<Token %s %s: %s>" % (self.token_type, tuple(self.flags),
+                                      self.token)
+
+
+def regex_format_op(op):
+    if op[0].isalpha():
+        return re.escape(op) + r'\b'
+    else:
+        return re.escape(op)
+
+
+def build_op_regex(ops):
+    ops_pattern = ("(%s)(.*)" %
+                   '|'.join(regex_format_op(op) for op in ops))
+    return re.compile(ops_pattern, re.I)
+
+infix_ops_re = build_op_regex(and_ops | or_ops)
+# dash is not a proper prefix op. No following space allowed.
+dash_re = re.compile(r'(-)(\S.*)')
+prefix_ops_re = build_op_regex(not_ops - Set(['-']))
+ws_re = re.compile(r'\s+(.*)')
+quoted_phrase_re = re.compile(r'\"\s*(.*)\s*\"(.*)')
+plus_re = re.compile(r'(\+\S*)(.*)')
+raw_term_pattern = r'[0-9A-Za-z]+'
+term_re = re.compile(r'(%s)(.*)' % raw_term_pattern)
+
+token_peekable = peekable(Token('eof', ''))
+
+
+@token_peekable
+def tokenize(input_query):
+    current = input_query
+    while(current != ''):
+        # eat ws
+        ws_match = ws_re.match(current)
+        if ws_match:
+            current = ws_match.group(1)
+            continue
+
+        # try to match an open paren
+        if current[0] == '(':
+            # If there is no possible matching paren, it's a term
+            if ')' in input_query:
+                yield Token('openp', '(')
+            else:
+                yield Token('term', '(')
+            current = current[1:]
+            continue
+
+        # try to match a close paren
+        if current[0] == ')':
+            # If there is no possible matching paren, it's a term
+            if '(' in input_query:
+                yield Token('closep', ')')
+            else:
+                yield Token('term', ')')
+            current = current[1:]
+            continue
+
+        # try to match a quoted phrase
+        match = quoted_phrase_re.match(current)
+        if match:
+            current = match.group(2)
+            yield Token('term', match.group(1), ['quote'])
+            continue
+
+        # try to match an infix operator
+        match = infix_ops_re.match(current)
+        if match:
+            current = match.group(2)
+            yield Token('op', match.group(1).lower())
+            continue
+
+        # try to match prefix - special behavior
+        match = dash_re.match(current)
+        if match:
+            current = match.group(2)
+            yield Token('op', match.group(1).lower())
+            continue
+
+        # try to match a prefix operator
+        match = prefix_ops_re.match(current)
+        if match:
+            current = match.group(2)
+            yield Token('op', match.group(1).lower())
+            continue
+
+        # try to match unary + special behavior
+        match = plus_re.match(current)
+        if match:
+            current = match.group(2)
+            yield Token('term', match.group(1).lower())
+            continue
+
+        # try to match a term
+        match = term_re.match(current)
+        if match:
+            current = match.group(2)
+            yield Token('term', match.group(1))
+            continue
+
+        # pass it through as a single character term
+        yield Token('term', current[0])
+        current = current[1:]
+
+
+# Parser --------------------------------------
+
+class AstTree(object):
+    """The result of both parsing and optimizing.
+
+    This object has a type, children, and flags.
+
+    It is primarily responsible for representing value and serization.
+    """
+
+    def __init__(self, node_type, head=None, tail=[], flags=[], children=None):
+        self.node_type = node_type
+        if children is not None:
+            self.children = children
+        else:
+            self.children = [head] + tail
+        self.flags = flags
+
+    def string(self):
+        if self.node_type == 'or':
+            return "%s" % " OR ".join(self.child_strings())
+        elif self.node_type == 'and':
+            return " AND ".join(self.child_strings())
+        elif self.node_type == 'not':
+            return "NOT %s" % self.head_string()
+        elif self.node_type == 'term':
+            if "quote" in self.flags:
+                return '"%s"' % self.head_string()
+            else:
+                return self.head_string()
+        elif self.node_type == 'paren':
+            return "(%s)" % self.head_string()
+
+    def child_strings(self):
+        return (c.string() for c in self.children)
+
+    def head_string(self):
+        head = self.children[0]
+        if (isinstance(head, AstTree)):
+            return head.string()
+        else:
+            return head
+
+    def __repr__(self):
+        return "<AstTree %s %s: %s>" % (self.node_type,
+                                        tuple(self.flags),
+                                        self.children)
+
+
+class Energy(object):
+    def __init__(self, starting_energy):
+        self.starting_energy = starting_energy
+        self.energy = starting_energy
+
+    def step(self):
+        self.energy -= 1
+        if self.energy <= 0:
+            message = ("Too many parsing steps: %d."
+                       % self.starting_energy)
+            raise ValueError(message)
+
+
+class Parser(object):
+    def __init__(self, token_stream, stepper):
+        self.token_stream = token_stream
+        self.stepper = stepper
+
+    def parse(self):
+        """Process tokens, and produce an AST."""
+        return self.handle_and()
+
+    def handle_and(self):
+        self.stepper.step()
+        first = self.handle_or()
+        additional = []
+        while self.token_stream.peek().is_andable():
+            while self.token_stream.peek().is_and():
+                self.token_stream.next()
+            if self.token_stream.peek().is_eof():
+                break
+            additional.append(self.handle_or())
+        if len(additional) > 0:
+            return AstTree('and', first, additional)
+        else:
+            return first
+
+    def handle_or(self):
+        self.stepper.step()
+        first = self.handle_not()
+        additional = []
+        while self.token_stream.peek().is_or():
+            while self.token_stream.peek().is_or():
+                self.token_stream.next()
+            if self.token_stream.peek().is_eof():
+                break
+            additional.append(self.handle_not())
+        if len(additional) > 0:
+            return AstTree('paren', AstTree('or', first, additional))
+        else:
+            return first
+
+    def handle_not(self):
+        self.stepper.step()
+        has_not_op = False
+        if self.token_stream.peek().is_not():
+            self.token_stream.next()
+            has_not_op = True
+        term = self.handle_paren()
+        if has_not_op:
+            return AstTree('not', term)
+        else:
+            return term
+
+    def handle_paren(self):
+        self.stepper.step()
+        found_open = False
+        found_close = False
+
+        if self.token_stream.peek().is_openparen():
+            self.token_stream.next()
+            found_open = True
+
+        if found_open:
+            term = self.handle_and()
+        else:
+            term = self.handle_term()
+
+        if found_open and self.token_stream.peek().is_closeparen():
+            self.token_stream.next()
+            found_close = True
+
+        if found_open and found_close:
+            return AstTree('paren', term)
+        else:
+            return term
+
+    def handle_term(self):
+        self.stepper.step()
+        # Eat any garbage
+        while self.token_stream.peek().token_type not in ('term', 'eof'):
+            token = self.token_stream.next()
+        token = self.token_stream.next()
+        return AstTree('term', token.token, flags=token.flags)
+
+
+# Optimizer ----------------------------------
+
+# optimize_tree coordinates the work.
+#
+# The actual work is done by the optimization functions.
+#
+# Each function receives a reference to the tree node which is being
+# optimized and a reference to the root node of the entire tree. A few
+# patterns need that root reference.
+#
+# The function is responsible for inspecting the node to determine if
+# it should do anything with the node.
+#
+# The function, if it determines it should modify the node, should do so,
+# and return None.
+#
+# If the function needs to replace the current node altogether, it should
+# return a new node. The caller is responsible for doing the replacement.
+#
+# To add new patterns, write a new optimization function and add a
+# reference to it in optimize_tree.
+
+
+def remove_root_quotes(tree, root):
+    if (tree is root and tree.node_type == 'term' and
+            'quote' in tree.flags):
+        tree.flags.remove('quote')
+
+
+bare_term_re = re.compile(r'\A%s\Z' % raw_term_pattern)
+
+
+def remove_simple_term_quotes(tree, root):
+    if (tree.node_type == 'term' and
+            'quote' in tree.flags and
+            bare_term_re.match(tree.children[0])):
+        tree.flags.remove('quote')
+
+
+def drop_standalone_dash(tree, root):
+    if (tree.node_type == 'and' and
+            len(tree.children) == 2 and
+            isinstance(tree.children[0], AstTree) and
+            isinstance(tree.children[1], AstTree) and
+            tree.children[0].node_type == 'term' and
+            tree.children[0].children[0] == '-' and
+            tree.children[1].node_type == 'term'):
+        children = [" ".join(child.children[0]
+                    for child in tree.children[1:])]
+
+        new_children = [" ".join(child.children[0]
+                        for child in tree.children[1:])]
+        return AstTree('term', children=new_children)
+
+
+def prepend_term_prefix(tree, root):
+    if (tree.node_type == 'and' and
+            len(tree.children) > 1 and
+            tree.children[0].node_type == 'term' and
+            tree.children[1].node_type == 'term' and
+            tree.children[1].flags == [] and
+            tree.children[0].children[0] in ['(', '"', '*', '?']):
+        new_children = [
+            AstTree('term', "%s%s" %
+                    (tree.children[0].children[0],
+                     tree.children[1].children[0]))]
+        new_children.extend(tree.children[2:])
+        return AstTree('and', children=new_children)
+
+
+def append_term_suffix(tree, root):
+    if (tree.node_type == 'and' and
+            len(tree.children) > 1 and
+            tree.children[-2].node_type == 'term' and
+            tree.children[-1].node_type == 'term' and
+            tree.children[-2].flags == [] and
+            tree.children[-1].children[0] in ['"', '*', '?', ')']):
+        new_children = tree.children[:-2]
+        new_children.append(
+            AstTree('term', "%s%s" % (
+                     tree.children[-2].children[0],
+                     tree.children[-1].children[0])))
+        return AstTree('and', children=new_children)
+
+
+def remove_redundant_parens(tree, root):
+    if (tree.node_type == 'paren' and
+            len(tree.children) == 1 and
+            isinstance(tree.children[0], AstTree) and
+            tree.children[0].node_type == 'paren'):
+        tree.children = tree.children[0].children
+
+
+def escape_prefix_symbol(tree, root):
+    if (tree.node_type == 'term' and
+            len(tree.children[0]) > 0 and
+            tree.children[0][0] != '+' and
+            tree.children[0][0] in ['*', '(']):
+        tree.children[0] = '\\%s' % tree.children[0]
+
+
+def escape_suffix_symbol(tree, root):
+    if (tree.node_type == 'term' and
+            len(tree.children[0]) > 0 and
+            tree.children[0][0] != '+' and
+            tree.children[0][-1] in [')']):
+        original = tree.children[0]
+        edited = '%s\\%s' % (original[:-1], original[-1])
+        tree.children[0] = edited
+
+
+def escape_plus_term(tree, root):
+    if (tree.node_type == 'term' and
+            len(tree.children[0]) > 0 and
+            tree.children[0][0] == '+'):
+        result = tree.children[0]
+        single_chars_regex = '([%s])' % re.escape('+-!(){}[]^~*?:')
+        result = re.sub(single_chars_regex, r'\\\1', result)
+        result = re.sub(r'(&+|\|+)', r'\\\1', result)
+        tree.children[0] = result
+
+
+def optimize_tree(tree, root):
+    optimizations = [
+        remove_root_quotes,
+        remove_simple_term_quotes,
+        remove_redundant_parens,
+        drop_standalone_dash,
+        prepend_term_prefix,
+        append_term_suffix,
+        escape_prefix_symbol,
+        escape_suffix_symbol,
+        escape_plus_term,
+    ]
+
+    for optimization in optimizations:
+        new_tree = optimization(tree, root)
+        if new_tree is not None:
+            tree = new_tree
+
+    for i, child in enumerate(tree.children):
+        if isinstance(child, AstTree):
+            new_child = optimize_tree(child, root)
+            if new_child is not None:
+                tree.children[i] = new_child
+
+    return tree
+
+
+# Entry Point -------------------------------------
+
+class SearchTransformer(object):
+    def __init__(self, tokenize, parser, optimize, stepper_factory):
+        self.tokenize = tokenize
+        self.parser = parser
+        self.optimize = optimize
+        self.stepper_factory = stepper_factory
+
+    def transform(self, input_query, location, default_query):
+        try:
+            # Tokenize
+            token_stream = self.tokenize(input_query)
+
+            # Parse
+            stepper = self.stepper_factory()
+            parser = self.parser(token_stream, stepper)
+            tree = parser.parse()
+
+            # Optimize
+            optimized_tree = self.optimize(tree, tree)
+
+            # Serialize
+            query = optimized_tree.string()
+
+            # Stuff in other queries if needed
+            aux_queries = [default_query, location]
+            if any(q != '' for q in aux_queries):
+                all_queries = (q for q in aux_queries + [query] if q != '')
+                return " AND ".join("(%s)" % q for q in all_queries)
+            else:
+                return query
+        except Exception as e:
+            logging.warn("Could not handle search '%s': %s" %
+                         (input_query, e.message))
+            return input_query
+
+
+def transform_search(input_query, location, default_query):
+    """Create a query ready for sending to Solr."""
+    def build_stepper():
+        return Energy(5000)
+
+    compiler = SearchTransformer(tokenize, Parser, optimize_tree,
+                                 build_stepper)
+    return compiler.transform(input_query, location, default_query)
---
+++ seo/tests/test_search_transformer.py
@@ -0,0 +1,148 @@
+# from django.test import TestCase
+from unittest import TestCase
+import logging
+import __main__
+
+from seo.search_transformer import transform_search, \
+    SearchTransformer, optimize_tree, Parser, Energy, \
+    token_peekable, tokenize
+
+# Columns:
+# input query, input location, default_query, expected output
+test_data = [
+    ('teaching assistant', '', '', 'teaching AND assistant'),
+    ('teaching and assistant', '', '', 'teaching AND assistant'),
+    ('teaching & assistant', '', '', 'teaching AND assistant'),
+    ('teaching &      assistant', '', '', 'teaching AND assistant'),
+    ('teaching     &    assistant', '', '', 'teaching AND assistant'),
+    ('teaching && assistant', '', '', 'teaching AND assistant'),
+    ('teaching AND assistant', '', '', 'teaching AND assistant'),
+    ('teaching AND "assistant"', '', '', 'teaching AND assistant'),
+    ('teaching,assistant', '', '', '(teaching OR assistant)'),
+    ('teaching ,assistant', '', '', '(teaching OR assistant)'),
+    ('teaching or assistant', '', '', '(teaching OR assistant)'),
+    ('teaching OR assistant', '', '', '(teaching OR assistant)'),
+    ('teaching | assistant', '', '', '(teaching OR assistant)'),
+    ('teaching || assistant', '', '', '(teaching OR assistant)'),
+    ('teaching   or  assistant', '', '', '(teaching OR assistant)'),
+    ('"teaching OR assistant"', '', '', 'teaching OR assistant'),
+    ('"teaching,assistant"', '', '', 'teaching,assistant'),
+    ('"teaching assistant"', '', '', 'teaching assistant'),
+    ('" teaching assistant"', '', '', 'teaching assistant'),
+    ('"    teaching assistant"', '', '', 'teaching assistant'),
+    ('!"teaching assistant"', '', '', 'NOT "teaching assistant"'),
+    ('NOT "teaching assistant"', '', '', 'NOT "teaching assistant"'),
+    ('not "teaching assistant"', '', '', 'NOT "teaching assistant"'),
+    ('-"teaching assistant"', '', '', 'NOT "teaching assistant"'),
+    ('!teacher', '', '', 'NOT teacher'),
+    ('NOT teacher', '', '', 'NOT teacher'),
+    ('not teacher', '', '', 'NOT teacher'),
+    ('-teacher', '', '', 'NOT teacher'),
+    ('! teacher', '', '', 'NOT teacher'),
+    ('!    teacher', '', '', 'NOT teacher'),
+    ('NOT   teacher', '', '', 'NOT teacher'),
+    ('not   teacher', '', '', 'NOT teacher'),
+    ('- teacher', '', '', 'teacher'),
+    ('& assistant', '', '', 'assistant'),
+    ('assistant &', '', '', 'assistant'),
+    ('| assistant', '', '', 'assistant'),
+    ('assistant |', '', '', 'assistant'),
+    ('AND assistant', '', '', 'assistant'),
+    ('assistant AND', '', '', 'assistant'),
+    ('OR assistant', '', '', 'assistant'),
+    ('assistant OR', '', '', 'assistant'),
+    ('"AND assistant"', '', '', 'AND assistant'),
+    ('"assistant AND"', '', '', 'assistant AND'),
+    ('"OR assistant"', '', '', 'OR assistant'),
+    ('"assistant OR"', '', '', 'assistant OR'),
+    ('(assistant OR teacher)', '', '', '(assistant OR teacher)'),
+    ('(assistant OR teacher) and curriculum', '', '',
+        '(assistant OR teacher) AND curriculum'),
+    ('(assistant AND teacher) OR curriculum', '', '',
+        '((assistant AND teacher) OR curriculum)'),
+    ('(assistant AND teacher) OR curriculum OR (homework AND test)', '',
+        '',
+        '((assistant AND teacher) OR curriculum OR (homework AND test))'),
+    ("+-&&||!(){}[]^\"~*?:\\", '', '',
+        '\+\-\&&\||\!\(\)\{\}\[\]\^\"\~\*\?\:\\'),
+    ('teach*', '', '', 'teach*'),
+    ('teach?', '', '', 'teach?'),
+    ('"teaching assistant', '', '', '\"teaching AND assistant'),
+    ('(teaching assistant', '', '', '\(teaching AND assistant'),
+    ('teaching assistant"', '', '', 'teaching AND assistant\"'),
+    ('teaching assistant)', '', '', 'teaching AND assistant\)'),
+    ('*teach', '', '', '\*teach'),
+    ('query', '', 'defaultquery', '(defaultquery) AND (query)'),
+    ('query', 'locationquery', '', '(locationquery) AND (query)'),
+    ('query', 'locationquery', 'defaultquery',
+        '(defaultquery) AND (locationquery) AND (query)'),
+
+    # Cases added after the official cases in PD-616
+    ('', '', '', ''),
+
+]
+
+
+# Dynamically add data driven tests to this class so that we always
+# get results for all the test cases, even if some fail.
+class TestSearchParser(TestCase):
+    pass
+
+
+def test_one_case(self, input_query, location, default_query, expected):
+    actual = transform_search(input_query, location, default_query)
+    message_data = [input_query, location, default_query]
+    message = ""
+    message += "\n    input: "
+    message += " | ".join([d for d in message_data if d != ''])
+    message += "\n expected: "
+    message += expected
+    message += "\n      got: "
+    message += str(actual)
+    self.assertEqual(expected, actual, message)
+    logging.info("Correct: %s -> %s", input_query, expected)
+
+for i, (input_query, location, default_query, expected) \
+        in enumerate(test_data):
+
+    def run_test(self, input_query=input_query, location=location,
+                 default_query=default_query, expected=expected):
+        test_one_case(self, input_query, location, default_query, expected)
+    setattr(TestSearchParser, 'test_%03d' % (i + 1),
+            run_test)
+
+
+class TestSearchParserSpecial(TestCase):
+    def test_internal_error(self):
+
+        @token_peekable
+        def blow_up(stream):
+            raise Exception
+            yield None
+
+        def energy_factory():
+            return Energy(5000)
+
+        transformer = SearchTransformer(blow_up, Parser, optimize_tree,
+                                        energy_factory)
+        result = transformer.transform("error here ok", "", "")
+        self.assertEqual("error here ok", result)
+
+    def test_too_much_parsing(self):
+        def energy_factory():
+            return Energy(5)
+
+        transformer = SearchTransformer(tokenize, Parser, optimize_tree,
+                                        energy_factory)
+
+        bad_query = 10 * "happy "
+        result = transformer.transform(bad_query, "", "")
+        self.assertEqual(bad_query, result)
+
+# Run just this test very fast:
+# PYTHONPATH=.:seo/tests python -m unittest test_search_transformer
+# or just a single test:
+# PYTHONPATH=.:seo/tests python -m unittest \
+#   test_search_transformer.TestSearchParser.test_002
+if 'unittest/__main__' in __main__.__file__:
+    logging.basicConfig(level=logging.INFO)
