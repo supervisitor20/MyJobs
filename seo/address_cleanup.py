473f632c64940c29b4a9c18f9b29450f2e671254
Jon Koons <jkoons@apps.directemployers.org>
2015-09-11 08:25:11 -0400

    more stuff

--- seo/address_cleanup.py
+++ seo/address_cleanup.py
@@ -1,88 +1,83 @@
 import re
-address_line_1_terms = ['Alley','Aly',
-                        'Arcade','Arc',
-                        'Avenue','Ave',
-                        'Beach','Bch',
-                        'Bend','Bnd',
-                        'Bottom','Btm',
-                        'Boulevard','Blvd',
-                        'Branch','Br',
-                        'Bypass','Byp',
-                        'Camp','Cp',
-                        'Causeway','Cswy',
-                        'Center','Ctr',
-                        'Circle','Cir',
-                        'Court','Ct',
-                        'Cove','Cv',
-                        'Creek','Crk',
-                        'Crossing','Xing',
-                        'Crossroad','Xrd',
-                        'Drive','Dr',
-                        'East','E',
-                        'Expressway','Expy',
-                        'Field','Fld',
-                        'Freeway','Fwy',
-                        'Front','Frnt',
-                        'Gateway','Gtwy',
-                        'Harbor','Hbr',
-                        'Heights','Hts',
-                        'Highway','Hwy',
-                        'Island','Is',
-                        'Junction','Jct',
-                        'Lake','Lk',
-                        'Lane','Ln',
-                        'Mount','Mt',
-                        'NE','Northeast',
-                        'North','N',
-                        'NW','Northwest',
-                        'Parkway','Pky',
-                        'Place','Pl',
-                        'Plain','Pln',
-                        'Plaza','Plz',
-                        'Point','Pt',
-                        'Ranch','Rnch',
-                        'Rapids','Rpds',
-                        'Ridge','Rdg',
-                        'Road','Rd',
-                        'Route','Rte',
-                        'SE','Southeast',
-                        'Skyway','Skwy',
-                        'South','S',
-                        'Space','Spc',
-                        'Spring','Spg',
-                        'Square','Sq',
-                        'Station','Sta',
-                        'Stravenue','Stra',
-                        'Street','St',
-                        'Summit','Smt',
-                        'SW','Southwest',
-                        'Terrace','Ter',
-                        'Trace','Trce',
-                        'Turnpike','Tpk',
-                        'Valley','Vly',
-                        'View','Vw',
-                        'Village','Vlg',
-                        'Well','Wl',
-                        'West','W',
-                        'Apartment','Apt',
-                        'Building','Bldg',
-                        'Office','Ofc',
-                        'Room','Rm',
-                        'Suite','Ste',
-                        'Trailer','Trlr',
-                        'Lobby','Lbby',
-                        'Hangar','Hngr',
-                        'Floor','Fl',
-                        'Basement','Bsmt',
-                        'PO','P.O.']
+address_line_1_terms = ['alley','aly',
+                        'avenue','ave',
+                        'beach','bch',
+                        'bend','bnd',
+                        'boulevard','blvd',
+                        'bypass','byp',
+                        'causeway','cswy',
+                        'center','ctr',
+                        'circle','cir',
+                        'court','ct',
+                        'cove','cv',
+                        'creek','crk',
+                        'crossing','xing',
+                        'crossroad','xrd',
+                        'drive','dr',
+                        'expressway','expy',
+                        'freeway','fwy',
+                        'front','frnt',
+                        'gateway','gtwy',
+                        'harbor','hbr',
+                        'heights','hts',
+                        'highway','hwy',
+                        'junction','jct',
+                        'lake','lk',
+                        'lane','ln',
+                        'parkway','pky',
+                        'place','pl',
+                        'plain','pln',
+                        'plaza','plz',
+                        'point','pt',
+                        'ranch','rnch',
+                        'ridge','rdg',
+                        'road','rd',
+                        'route','rte',
+                        'skyway','skwy',
+                        'square','sq',
+                        'station','sta',
+                        'stravenue','stra',
+                        'street','st',
+                        'summit','smt',
+                        'terrace','ter',
+                        'turnpike','tpk',
+                        'view','vw']
 
-re_beginning_number = re.compile("^(d+)(.*)")
+address_line_2_terms = ['apartment','apt',
+                        'building','bldg',
+                        'office','ofc',
+                        'room','rm',
+                        'suite','ste',
+                        'trailer','trlr',
+                        'lobby','lbby',
+                        'hangar','hngr',
+                        'floor','fl',
+                        'basement','bsmt',
+                        'po','p.o.','pobox']
+
+re_beginning_number = re.compile("^(\d+)")
+re_address_string = re.compile("s+(%s)s+" % "|".join(address_line_1_terms + address_line_2_terms))
+re_address = re.compile("^(\d+)\s+\S+.*\s+(%s)\s" % "|".join(address_line_1_terms))
 
 def identify_address(input_string):
-    address_score = 0
-    input_string = input_string.strip()
+    properties = []
+    input_string = input_string.strip().lower()
     match = re_beginning_number.match(input_string)
     if match:
-        address_score += 10
+        properties.append('begnumber')
+    else:
+        properties.append('nobeg')
 
-    
+    match = re_address_string.match(input_string)
+    if match:
+        properties.append('keywordfound')
+    else:
+        properties.append('nokw')
+
+    match = re_address.match(input_string)
+    if match:
+        properties.append('formatmatch')
+    else:
+        properties.append('noformat')
+
+    return properties
