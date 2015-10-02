192ddce8ea344a63428b4c574b5bca60ae43c97e
Darrin Thompson <dthompson@apps.directemployers.org>
2015-10-02 11:18:42 -0400

    Clean up test more.

--- myblocks/tests/test_secure_blocks.py
+++ myblocks/tests/test_secure_blocks.py
@@ -2,17 +2,15 @@ import json
 
 from django.core.urlresolvers import reverse
 
-from myjobs.tests.setup import MyJobsBase
+from seo.tests.setup import DirectSEOBase
 
 from seo.tests.factories import SeoSiteFactory
 
 
-class TestChildDashboard(MyJobsBase):
+class TestChildDashboard(DirectSEOBase):
     def test_secure_blocks(self):
-
         url = reverse('secure_blocks')
-        site = SeoSiteFactory(domain='jobs.example.com')
-        site.save()
+        SeoSiteFactory(domain='jobs.example.com')
         resp = self.client.post(
             url, '{"blocks": {}}',
             HTTP_HOST='jobs.example.com',
