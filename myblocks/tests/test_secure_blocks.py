fa1951b31758fc871f2994d2913bda791c7ad295
Darrin Thompson <dthompson@apps.directemployers.org>
2015-10-02 11:02:01 -0400

    Make heisentest stable.

--- myblocks/tests/test_secure_blocks.py
+++ myblocks/tests/test_secure_blocks.py
@@ -4,14 +4,19 @@ from django.core.urlresolvers import reverse
 
 from myjobs.tests.setup import MyJobsBase
 
+from seo.tests.factories import SeoSiteFactory
+
 
 class TestChildDashboard(MyJobsBase):
     def test_secure_blocks(self):
+
         url = reverse('secure_blocks')
+        site = SeoSiteFactory(domain='jobs.example.com')
+        site.save()
         resp = self.client.post(
             url, '{"blocks": {}}',
-            HTTP_HOST='jobs.directemployers.org',
-            HTTP_ORIGIN='http://jobs.directemployers.org',
+            HTTP_HOST='jobs.example.com',
+            HTTP_ORIGIN='http://jobs.example.com',
             HTTP_X_REQUESTED_WITH='XMLHttpRequest',
             content_type="application/json",
         )
