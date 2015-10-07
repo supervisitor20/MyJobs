001b6826c28d84fff51515f7e57bc478eae13b58
Darrin Thompson <dthompson@apps.directemployers.org>
2015-10-07 15:39:41 -0400

    Add a few more tests for secure blocks view.

--- myblocks/tests/test_secure_blocks.py
+++ myblocks/tests/test_secure_blocks.py
@@ -8,28 +8,33 @@ from seo.tests.factories import SeoSiteFactory
 
 
 class TestSecureBlocks(DirectSEOBase):
-    def test_secure_blocks(self):
-        url = reverse('secure_blocks')
+    fixtures = ['login_page.json']
+
+    def test_secure_blocks_empty(self):
         SeoSiteFactory(domain='jobs.example.com')
-        resp = self.client.post(
-            url, '{"blocks": {}}',
-            HTTP_HOST='jobs.example.com',
-            HTTP_ORIGIN='http://jobs.example.com',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
-            content_type="application/json",
-        )
+        resp = self.make_sb_request('{"blocks": {}}')
         self.assertEqual(200, resp.status_code)
         result = json.loads(resp.content)
         self.assertEqual({}, result)
 
     def test_secure_blocks_bad_parse(self):
-        url = reverse('secure_blocks')
         SeoSiteFactory(domain='jobs.example.com')
-        resp = self.client.post(
-            url, '@@@@@@@@@',
+        resp = self.make_sb_request('@@@@@@@@@')
+        self.assertEqual(400, resp.status_code)
+
+    def test_secure_blocks_render(self):
+        SeoSiteFactory(domain='jobs.example.com')
+        body = '{"blocks": {"my-jobs-logo-1": {}}}'
+        resp = self.make_sb_request(body)
+        result = json.loads(resp.content)
+        self.assertTrue('my-jobs-logo-1' in result)
+
+    def make_sb_request(self, body):
+        url = reverse('secure_blocks')
+        return self.client.post(
+            url, body,
             HTTP_HOST='jobs.example.com',
             HTTP_ORIGIN='http://jobs.example.com',
             HTTP_X_REQUESTED_WITH='XMLHttpRequest',
             content_type="application/json",
         )
-        self.assertEqual(400, resp.status_code)
