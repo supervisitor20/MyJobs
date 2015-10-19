6b4781b5f5e1620d653e77573ded1c143ba2cfa3
Jon Koons <jkoons@apps.directemployers.org>
2015-10-19 09:32:14 -0400

    added print to PSS forms

--- mysearches/forms.py
+++ mysearches/forms.py
@@ -263,6 +263,7 @@ class PartnerSavedSearchForm(RequestForm):
     def save(self, commit=True):
         self.instance.feed = self.cleaned_data.get('feed')
         self.instance.update_last_action_time(False)
+        print 'PSS FORM SAVED: ', self.instance.last_action_time
         is_new_or_change = CHANGE if self.instance.pk else ADDITION
         if not self.instance.pk:
             self.instance.sort_by = 'Date'
@@ -313,4 +314,5 @@ class PartnerSubSavedSearchForm(RequestForm):
 
     def save(self, commit=True):
         self.instance.update_last_action_time(False)
+        print 'PSS SUB FORM SAVED: ', self.instance.last_action_time
         return super(PartnerSubSavedSearchForm, self).save()
