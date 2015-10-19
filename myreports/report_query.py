c1052804d1ce0faa9152dfa97dc2486c5c8023cc
Darrin Thompson <dthompson@apps.directemployers.org>
2015-10-19 11:01:41 -0400

    Document report_query module.

--- myreports/report_query.py
+++ myreports/report_query.py
@@ -1,3 +1,38 @@
+"""Manage queries associated with report types.
+
+This module contains query handlers and utilities for organizing and finding
+them by name. The presumption in this module is that there is a list of named
+report types in the database. We associate handlers by those names here.
+
+A handler is repsponsible for:
+    * generating an appropriate query set
+    * converting records to plain python dictionaries and lists, suitable for
+      conversion to json.
+    * generating a list of available columns
+
+See Also: get_report_query and @report_query.
+
+Example:
+
+>>> @report_query("PRM")
+>>> class PRMReportType(object):
+        fields = ['name', 'email', 'locations', ...]
+
+        def report_query_set(self, company):
+            # return a queryset.
+            # Include any prefetch_related optimizations here.
+            pass
+
+        def extract(self, rec):
+            # rec is a model. Returns a dict.
+            # Handle any foreign key expansion in this method.
+            pass
+
+
+>>> get_report_query("PRM")
+# returns reference to class PRMReportType
+
+"""
 from mypartners.models import Status, Contact
 
 
@@ -5,6 +40,10 @@ report_queries = {}
 
 
 def report_query(report_query_name):
+    """Register this class as a report query handler.
+
+    See also module report_query for interface.
+    """
     def register(report_query):
         report_queries[report_query_name] = report_query
         return report_query
@@ -12,6 +51,7 @@ def report_query(report_query_name):
 
 
 def get_report_query(report_query_name):
+    """Return the class associated with this query name."""
     return report_queries[report_query_name]()
 
 
