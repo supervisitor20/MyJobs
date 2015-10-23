668ec74bc352c751563d3d6f2e3ce0496c367033
aspidites <emarshall85@gmail.com>
2015-10-23 10:21:28 -0400

    Role Admin -> Admin

--- myjobs/migrations/0007_create_admin_roles.py
+++ myjobs/migrations/0007_create_admin_roles.py
@@ -7,11 +7,11 @@ from django.db import models
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        """Create a 'Role Admin' role for existing companies."""
+        """Create a 'Admin' role for existing companies."""
 
         # create the required roles
         orm.Role.objects.bulk_create([
-            orm.Role(name="Role Admin", company=company)
+            orm.Role(name="Admin", company=company)
             for company in orm["seo.Company"].objects.all()])
 
         roles = orm.Role.objects.all()
