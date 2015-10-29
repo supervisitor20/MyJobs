b3017f645576cdcc917fdeb241868f323f8acb38
aspidites <emarshall85@gmail.com>
2015-10-29 09:16:43 -0400

    Refactored a few things and added the enabling of PRM.

--- myjobs/management/commands/create_prm_users.py
+++ myjobs/management/commands/create_prm_users.py
@@ -1,24 +1,20 @@
 import sys
 from django.core.management.base import BaseCommand
+from django.db.models import F
 from south.models import MigrationHistory
 
 from seo.models import Company, CompanyUser
-from myjobs.models import User, Role, Activity
+from myjobs.models import User, Role, Activity, AppAccess
 
 
 class Command(BaseCommand):
-    help = "Import Partners, Contacts, and ContactRecords from a JSON files."
+    help = "Create PRM Users and enable PRM for member companies."
 
-    def handle(self, *args, **options):
-        if not MigrationHistory.objects.filter(
-                migration="0007_create_admin_roles").exists():
-            sys.stderr.write(
-                "This command should be run after migration "
-                "`0007_create_admin_roles` has been applied. To do so, please "
-                "migrate myjobs forward.")
-            sys.exit(1)
-
-        company_users = CompanyUser.objects.all()
+    def _create_prm_user_roles(self, company_users):
+        """
+        Creates the "PRM User" role for all companies who have a company
+        user.
+        """
         companies = Company.objects.filter(pk__in=company_users.values_list(
             'company', flat=True))
 
@@ -26,7 +22,20 @@ class Command(BaseCommand):
         Role.objects.bulk_create([
             Role(name="PRM User", company=company) for company in companies])
 
-        # bulk create doesn't assign foreign keys when it's done
+        print "Created `PRM User` roles for %s companies." % companies.count()
+
+    def _assign_prm_users(self, company_users):
+        """
+        Assign company users the "PRM User" role if they aren't already
+        assigned the "Admin" role.
+        """
+        # filter out users who already admins
+        admins = company_users.filter(
+            company__role__user__email=F('user__email'),
+            company__role__name='Admin')
+
+        # bulk create doesn't assign foreign keys when it's done so we manually
+        # fetch them
         roles = {role.company: role
                  for role in Role.objects.filter(name="PRM User")}
         activities = Activity.objects.filter(app_access__name="PRM").distinct()
@@ -43,10 +52,64 @@ class Command(BaseCommand):
         UserRoles = User.roles.through
 
         # directly populate the many to many table to avoid additional queries
+        prm_users = company_users.exclude(pk__in=admins.values_list(
+            'pk', flat=True))
         UserRoles.objects.bulk_create([
             UserRoles(
                 role=roles[company_user.company], user=company_user.user)
-            for company_user in company_users])
+            for company_user in prm_users])
 
         print "Created %s PRM Users from %s Company Users" % (
             UserRoles.objects.count(), CompanyUser.objects.count())
+
+        company_user_count = company_users.count()
+        admin_count = admins.count()
+        if company_user_count != admin_count:
+            print ("%s company users(s) skipped as they were already "
+                   "assigned the Admin role." % admin_count)
+
+    def _enable_prm_access(self):
+        """
+        Add `PRM` app access to any company who is a member.
+        """
+
+        companies = Company.objects.filter(member=True)
+        disabled_companies = companies.exclude(app_access__name="PRM")
+        app_access = AppAccess.objects.get(name="PRM")
+
+        # maps to company_app_access
+        CompanyAppAccess = Company.app_access.through
+
+        # directly populate the many to many table to avoid additional queries
+        CompanyAppAccess.objects.bulk_create([
+            CompanyAppAccess(company=company, appaccess=app_access)
+            for company in disabled_companies])
+
+        print ("PRM access and reporting has been granted for %s "
+               "member companies." % (companies.count()))
+
+        company_count = companies.count()
+        disabled_count = disabled_companies.count()
+
+        if company_count != disabled_count:
+            skipped = company_count - disabled_count
+            print ("%s compan(y/ies) skipped because access to PRM and "
+                   "reporting was already granted." % skipped)
+
+
+
+    def handle(self, *args, **options):
+        if not MigrationHistory.objects.filter(
+                migration="0007_create_admin_roles").exists():
+            sys.stderr.write(
+                "This command should be run after migration "
+                "`0007_create_admin_roles` has been applied. To do so, please "
+                "migrate myjobs forward.")
+            sys.exit(1)
+
+        company_users = CompanyUser.objects.all()
+
+        self._create_prm_user_roles(company_users)
+        self._assign_prm_users(company_users)
+        self._enable_prm_access()
+
