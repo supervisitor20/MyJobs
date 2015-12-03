7f1f7ba6e2e68bf22b9193c4697291918871b9ab
aspidites <emarshall85@gmail.com>
2015-12-03 08:58:57 -0500

    gh-pages doesn't like djagno?

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-from django.conf import settings
+import settings
 
 # -- General configuration ------------------------------------------------
 
