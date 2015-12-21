2a16a6381eb613139adde0434196e9879e82e92a
misterjonscott <keepingyouawake@gmail.com>
2015-12-21 12:42:20 -0500

    Removed extraneous tests per Jason M's suggestion

--- seo/tests/test_views.py
+++ seo/tests/test_views.py
@@ -1742,62 +1742,6 @@ class SeoViewsTestCase(DirectSEOTestCase):
                                   '<meta itemprop="addressCountry" content="United States'
                                   , msg_prefix='Full location test is failing')
 
-        # Job lookup by guid, check for empty state case.
-        self.conn.delete(q="*:*")
-        solr_docs_copy = deepcopy(self.solr_docs)
-        kwargs1 = {
-            'id': 'seo.joblisting.1',
-            'state': ''
-        }
-        solr_docs_copy[0].update(kwargs1)
-        self.conn.add(solr_docs_copy)
-        resp = self.client.get(
-            u'/indianapolis-in/retail-associate-розничная-ассоциированных/11111111111111111111111111111111/job/',
-            HTTP_HOST='buckconsultants.jobs')
-        # import ipdb; ipdb.set_trace()
-        self.assertEqual(resp.status_code, 200, msg="Not receiving 200 response in test for no state")
-        self.assertContains(resp, 'itemtype="http://schema.org/PostalAddress">'
-                                  '<span itemprop="addressLocality">Indiandapolis</span>'
-                                  '<span itemprop="addressCountry">United States</span>'
-                                #   , msg_prefix='Location without state test is failing %s' % self.conn.search('guid:11111111111111111111111111111111').docs)
-                                  , msg_prefix='Location without state test is failing %s' % re.sub(r'[^\x00-\x7F]+',' ', resp.content))
-
-        # Job lookup by guid, check for empty city case.
-        self.conn.delete(q="*:*")
-        solr_docs_copy = deepcopy(self.solr_docs)
-        kwargs1 = {
-            'id': 'seo.joblisting.1',
-            'city': ''
-        }
-        solr_docs_copy[0].update(kwargs1)
-        self.conn.add(solr_docs_copy)
-        resp = self.client.get(
-            u'/indianapolis-in/retail-associate-розничная-ассоциированных/11111111111111111111111111111111/job/',
-            HTTP_HOST='buckconsultants.jobs')
-        self.assertEqual(resp.status_code, 200, msg="Not receiving 200 response in test for no city")
-        self.assertContains(resp, 'itemtype="http://schema.org/PostalAddress">'
-                                  '<span itemprop="addressRegion">Indiana</span>'
-                                  '<meta itemprop="addressCountry" content="United States'
-                                  , msg_prefix='Location without city test is failing')
-        
-        # Job lookup by guid, check for empty city and state case.
-        self.conn.delete(q="*:*")
-        solr_docs_copy = deepcopy(self.solr_docs)
-        kwargs1 = {
-            'id': 'seo.joblisting.1',
-            'city': None,
-            'state':''
-        }
-        solr_docs_copy[0].update(kwargs1)
-        self.conn.add(solr_docs_copy)
-        resp = self.client.get(
-            u'/indianapolis-in/retail-associate-розничная-ассоциированных/11111111111111111111111111111111/job/',
-            HTTP_HOST='buckconsultants.jobs')
-        self.assertEqual(resp.status_code, 200, msg="Not receiving 200 response in test for no city or state")
-        self.assertContains(resp, 'itemtype="http://schema.org/PostalAddress">'
-                                  '<span itemprop="addressCountry">United States</span></span>'
-                                  , msg_prefix='Location without city or state test is failing')
-
     def test_job_detail(self):
         """
         Test that job detail pages return no server errors and that objects
