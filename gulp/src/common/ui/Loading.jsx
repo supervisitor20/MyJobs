e361c219e6d756bb3644a10bcf21dde9dc4766ac
Darrin Thompson <dthompson@apps.directemployers.org>
2016-01-15 14:32:54 -0500

    Add spinner component to indicate busy state.

---
+++ gulp/src/common/ui/Loading.jsx
@@ -0,0 +1,44 @@
+/* global spinnerImg */
+import React, {Component} from 'react';
+
+
+export class Loading extends Component {
+  componentWillMount() {
+    this.mounted = true;
+    this.setState({
+      show: false,
+    });
+  }
+
+  componentDidMount() {
+    this.allowShow = true;
+    setTimeout(() => this.setShowIfAllowed(), 300);
+  }
+
+  componentWillUnmount() {
+    this.allowShow = false;
+  }
+
+  setShowIfAllowed() {
+    if (this.allowShow) {
+      this.setState({show: true});
+    }
+  }
+
+  render() {
+    const {show} = this.state;
+    if (show) {
+      return (
+        <img
+          src={spinnerImg}
+          style={{
+            align: 'center',
+            padding: '1em',
+          }}/>
+      );
+    }
+    return <span/>;
+  }
+}
+
+Loading.propTypes = {};
