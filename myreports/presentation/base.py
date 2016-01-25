394c5437f0f5a90ccbd7a920d242f7dfdf6fcadd
Darrin Thompson <dthompson@apps.directemployers.org>
2016-01-25 13:22:23 -0500

    Document base class for presentations.

--- myreports/presentation/base.py
+++ myreports/presentation/base.py
@@ -2,4 +2,48 @@ from abc import ABCMeta
 
 
 class Presentation:
+    """Base class for Reporting presentation types.
+
+    Implementations are responsible for selecting the content type and
+    filename extension which the user's browser will see at download time,
+    as well as writing bytes of the document to an output stream.
+    """
     ___metaclass__ = ABCMeta
+
+    def content_type(self):
+        """Content type as seen by the user's browser.
+
+        i.e. 'application/json', 'text/csv', etc.
+        """
+        pass
+
+    def filename_extension(self):
+        """Filename extension seen by the user at download time.
+
+        i.e. 'csv', 'xls', etc.
+        """
+        pass
+
+    def write_presentation(self, values, records, output):
+        """Write bytes representing the downloaded artifact.
+
+        values - Ordered list of columns to appear in the download.
+            i.e. ['a', 'c', 'b']
+        records - List of dicts containing data to appear in the download.
+            i.e. [
+            {'a': '1', 'b': '2', 'c', '3'},
+            {'a': '4', 'b': '5', 'c', '6'},
+        ],
+        output - has method output.write(bytes) which accepts bytes for
+            download
+
+        Example:
+
+            def write_presentation(self, values, records, output):
+                output.write('|'.join(values))
+                output.write('\\n')
+                for record in records:
+                    output.write("|".join(record[v] for v in values]))
+                    output.write('\\n')
+        """
+        pass
