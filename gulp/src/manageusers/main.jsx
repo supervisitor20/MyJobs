736d07eb84ce2779110fbc1496e6761359dcae16
Daniel Poynter <dpoynter@apps.directemployers.org>
2016-01-27 14:33:22 -0500

    add comment about refactoring

--- gulp/src/manageusers/main.jsx
+++ gulp/src/manageusers/main.jsx
@@ -60,6 +60,7 @@ export class App extends React.Component {
         }
       }
 
+      // TODO: Consider refactoring with lodash groupBy()
       // Build a table for each app present
       const tablesOfActivitiesByApp = [];
       // First assemble rows needed for each table
