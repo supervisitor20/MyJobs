aef5fd7c05191559854ed9d3095182d0997dbf74
Matt Lauber <github@mklauber.com>
2016-02-09 10:59:48 -0500

    Removed empty migration.

--- import_jobs/migrations/0001_initial.py
+++ import_jobs/migrations/0001_initial.py
@@ -8,13 +8,29 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'ImportRecord'
+        db.create_table(u'import_jobs_importrecord', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('buid', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, db_index=True, blank=True)),
+            ('success', self.gf('django.db.models.fields.BooleanField')()),
+        ))
+        db.send_create_signal(u'import_jobs', ['ImportRecord'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'ImportRecord'
+        db.delete_table(u'import_jobs_importrecord')
+
 
     models = {
-        
+        u'import_jobs.importrecord': {
+            'Meta': {'object_name': 'ImportRecord'},
+            'buid': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'db_index': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'success': ('django.db.models.fields.BooleanField', [], {})
+        }
     }
 
     complete_apps = ['import_jobs']
