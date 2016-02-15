6fd746666e698e2e5b400e6cb26038d2c5bb7a39
aspidites <emarshall85@gmail.com>
2016-02-15 12:04:43 -0500

    Changed the site lookup.

--- seo/lookups.py
+++ seo/lookups.py
@@ -1,7 +1,5 @@
 from ajax_select import register, LookupChannel
-from seo.models import Company
-
-from mypartners.admin import format_company_name
+from seo import models
 
 @register('companies')
 class CompaniesLookup(LookupChannel):
@@ -12,6 +10,31 @@ class CompaniesLookup(LookupChannel):
         return self.model.objects.filter(name__icontains=q).order_by('name')
 
     def format_match(self, company):
-        """Determines how autocomplete options look in the drop-down."""
-        return format_company_name(company)
+        """
+        Returns a string ot be used next to each company that displays how many
+        admins belong to that company, or a warning if there aren't any.
+
+        """
+        template = "{name} ({count} users){warning}"
+        count = company.company_user_count
+        warning = "" if count else " **Might be a duplicate**"
+
+        return template.format(name=company.name, count=count, warning=warning)
+
+@register('sites')
+class SitesLookup(LookupChannel):
+    model = models.SeoSite
+    min_length = 3
+
+    def get_query(self, q, request):
+        """Match on name or domain."""
+
+        return self.model.objects.filter(
+            Q(domain__startswith=q) | Q(name__startswith=q))[:10]
+
+    # See https://github.com/crucialfelix/django-ajax-selects/issues/153 for
+    # why this is necessary. Inherited models don't act correctly, so we help
+    # the framework out by manually returning results.
+    def get_objects(self, ids):
+        return list(self.model.objects.filter(pk__in=ids))
 
