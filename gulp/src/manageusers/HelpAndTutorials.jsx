3c438547d7eb9e15cdc81d4e6825c702a8ca18a2
Daniel Poynter <dpoynter@apps.directemployers.org>
2016-02-17 14:28:52 -0500

    change design

--- gulp/src/manageusers/HelpAndTutorials.jsx
+++ gulp/src/manageusers/HelpAndTutorials.jsx
@@ -11,10 +11,15 @@ class HelpAndTutorials extends React.Component {
           <div className="product-card-full no-highlight">
             <h3>PDF Tutorials</h3>
 
-            <p><a href="https://d2e48ltfsb5exy.cloudfront.net/content_mj/files/PRM+Admin+-+Quickly+add+a+User.pdf">Quickly add a user</a></p>
-            <p><a href="https://d2e48ltfsb5exy.cloudfront.net/content_mj/files/My.jobsUserGuide-UserManagement.pdf">General overview</a></p>
+            <p><i className="fa fa-file-pdf-o fa-2x pull-left"></i> <a href="https://d2e48ltfsb5exy.cloudfront.net/content_mj/files/PRM+Admin+-+Quickly+add+a+User.pdf">Quickly Add a User</a><br/>
+            This single page document covers the basics of adding a user to your company.</p>
+
+          <p><i className="fa fa-file-pdf-o fa-2x pull-left"></i> <a href="https://d2e48ltfsb5exy.cloudfront.net/content_mj/files/My.jobsUserGuide-UserManagement.pdf">General Overview</a><br/>
+            This comprehensive document will guide you through all the features of user management.</p>
 
             <h3>Video Overview</h3>
+
+            <p>This guided overview will walk you through all the features of user management.</p>
 
             <div className="video-embed-container">
               <iframe src="https://player.vimeo.com/video/155433118" frameBorder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
