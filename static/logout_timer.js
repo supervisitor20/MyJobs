89f5a6b874cafc6fe8d58ecfd69b7b72407ebe65
aspidites <emarshall85@gmail.com>
2016-02-25 11:59:55 -0500

    Take advantage of compress and remove unused file.

--- static/logout_timer.js
+++
@@ -1,8 +0,0 @@
-var readCookie = utils.readCookie;
-
-(window.setInterval(function() {
-  // if we are logged out and not already on the home page
-  if (readCookie('loggedout') === "True" && window.location.pathname !== "{% url 'home' %}") {
-    window.location.assign("{% url 'home' %}");
-  }
-}, 1000))();
