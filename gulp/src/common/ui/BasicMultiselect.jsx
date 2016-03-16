cd103e9fdb9f9ba0b1e8cd7c7f991923a157c9a2
Daniel Poynter <dpoynter@apps.directemployers.org>
2016-03-16 11:06:46 -0400

    add new React Components, reusable wherever we have forms

---
+++ gulp/src/common/ui/BasicMultiselect.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+class BasicMultiselect extends React.Component {
+  render() {
+    let requiredIndicator = '';
+    if (this.props.required) {
+      requiredIndicator = ' *';
+    }
+
+    let helpOrErrorText;
+    // Check if this field appears in the list of error messages passed down
+    if (this.props.errorMessages.hasOwnProperty(this.props.name)) {
+      helpOrErrorText = <span className="error-text">{this.props.errorMessages[this.props.name]}</span>;
+    } else if ((this.props.help_text)) {
+      helpOrErrorText = <span className="help-block">{this.props.help_text}</span>;
+    }
+
+    const options = [];
+
+    for (const option in this.props.choices) {
+      if (this.props.choices.hasOwnProperty(option)) {
+        options.push(<option value={this.props.choices[option].value} key={option}>{this.props.choices[option].display}</option>);
+      }
+    }
+
+    return (
+      <div className="row">
+        <div className="col-xs-12 col-md-4">
+          <lable>{this.props.label}{requiredIndicator}</lable>
+        </div>
+        <div className="col-xs-12 col-md-8">
+          <select
+            defaultValue={this.props.initial}
+            id={this.props.name}
+            required={this.props.required}
+            hidden={this.props.widget.is_hidden}
+            name={this.props.name}
+            onChange={this.props.onChange}>
+            {options}
+          </select>
+          {helpOrErrorText}
+        </div>
+      </div>
+    );
+  }
+}
+
+BasicMultiselect.propTypes = {
+  placeholder: React.PropTypes.string.isRequired,
+  initial: React.PropTypes.string.isRequired,
+  widget: React.PropTypes.object.isRequired,
+  label_suffix: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string.isRequired,
+  required: React.PropTypes.bool.isRequired,
+  onChange: React.PropTypes.func,
+
+  name: React.PropTypes.string.isRequired,
+  help_text: React.PropTypes.string.isRequired,
+  errorMessages: React.PropTypes.object.isRequired,
+  choices: React.PropTypes.array.isRequired,
+};
+
+BasicMultiselect.defaultProps = {
+  initial: '',
+  placeholder: '',
+  widget: {},
+  label_suffix: '',
+  label: '',
+  required: false,
+  name: '',
+  help_text: '',
+};
+
+export default BasicMultiselect;
---
+++ gulp/src/common/ui/BasicTextarea.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class BasicTextarea extends React.Component {
+  render() {
+    let requiredIndicator = '';
+    if (this.props.required) {
+      requiredIndicator = ' *';
+    }
+
+    let helpOrErrorText;
+    // Check if this field appears in the list of error messages passed down
+    if (this.props.errorMessages.hasOwnProperty(this.props.name)) {
+      helpOrErrorText = <span className="error-text">{this.props.errorMessages[this.props.name]}</span>;
+    } else if ((this.props.help_text)) {
+      helpOrErrorText = <span className="help-block">{this.props.help_text}</span>;
+    }
+
+    return (
+      <div className="row">
+        <div className="col-xs-12 col-md-4">
+          <lable>{this.props.label}{requiredIndicator}</lable>
+        </div>
+        <div className="col-xs-12 col-md-8">
+          <textarea
+            defaultValue={this.props.initial}
+            id={this.props.name}
+            name={this.props.name}
+            required={this.props.required}
+            hidden={this.props.widget.is_hidden}
+            placeholder={this.props.widget.attrs.placeholder}
+            rows={this.props.widget.attrs.rows}
+            cols={this.props.widget.attrs.cols}
+            onChange={this.props.onChange}
+            />
+          {helpOrErrorText}
+        </div>
+      </div>
+    );
+  }
+}
+
+BasicTextarea.propTypes = {
+  placeholder: React.PropTypes.string.isRequired,
+  initial: React.PropTypes.string.isRequired,
+  widget: React.PropTypes.object.isRequired,
+  label_suffix: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string.isRequired,
+  required: React.PropTypes.bool.isRequired,
+  onChange: React.PropTypes.func,
+  name: React.PropTypes.string.isRequired,
+  help_text: React.PropTypes.string.isRequired,
+  errorMessages: React.PropTypes.array.isRequired,
+};
+
+BasicTextarea.defaultProps = {
+  initial: '',
+  placeholder: '',
+  widget: {},
+  label_suffix: '',
+  label: '',
+  required: false,
+  name: '',
+  help_text: '',
+};
+
+export default BasicTextarea;
