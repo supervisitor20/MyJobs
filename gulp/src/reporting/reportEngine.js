27ed4b2ffa227b24761b48b047ef581bd074d383
Darrin Thompson <dthompson@apps.directemployers.org>
2016-03-23 16:09:32 -0400

    Improve docs.

--- gulp/src/reporting/reportEngine.js
+++ gulp/src/reporting/reportEngine.js
@@ -4,11 +4,13 @@ import {mapValues, assign} from 'lodash-compat/object';
 
 // This is the business logic of the myreports client.
 
-// Root of the client.
-//
-// Used to walk through the api needed to find a usable presentation type.
-//
-// Last call of the "walk" is buildReportConfiguration.
+/**
+ * Root of the client.
+ *
+ * Used to walk through the api needed to find a usable presentation type.
+ *
+ * Last call of the "walk" is buildReportConfiguration.
+ */
 export class ReportFinder {
   constructor(api, configBuilder) {
     this.api = api;
@@ -32,6 +34,14 @@ export class ReportFinder {
     return this.api.getPresentationTypes(reportTypeId, dataTypeId);
   }
 
+  /**
+   * Get a report configuration which can be customized and later run.
+   *
+   * TODO: change rpId to reportTypeDataType id.
+   * rpId: report presentation id
+   * onNameChanged, onFilterChange, onErrorsChanged:
+   *   see ReportConfiguration for definitions.
+   */
   async buildReportConfiguration(rpId, onNameChanged, onFilterChange,
       onErrorsChanged) {
     const filters = await this.api.getFilters(rpId);
@@ -44,19 +54,34 @@ export class ReportFinder {
       onErrorsChanged);
   }
 
+  /**
+   * Retrieve a list of reports by this user
+   * TODO: Handle searching, etc.
+   */
   async getReportList() {
     return await this.api.listReports();
   }
 
+  /**
+   * Submit a callback to be called any time there is a new report created.
+   *
+   * returns a reference which can be used later to unsubscribe.
+   */
   subscribeToReportList(callback) {
     this.newReportSubscribers[callback] = callback;
     return callback;
   }
 
+  /**
+   * Remove a callback from the new reports subscription.
+   */
   unsubscribeToReportList(ref) {
     delete this.newReportSubscribers[ref];
   }
 
+  /**
+   * Let subscribers know that there is a new report.
+   */
   noteNewReport(reportId) {
     for (const ref in this.newReportSubscribers) {
       if (this.newReportSubscribers.hasOwnProperty(ref)) {
@@ -66,23 +91,31 @@ export class ReportFinder {
   }
 }
 
-// Gradually build a filter for a report run with help from the api.
-//
-// Use the getHints function where available on fields to get sample
-// field values.
-//
-// Use setFilter to set simple filter values.
-//
-// Use the multifilter functions for filter values which are a collection of
-// items.
-//
-// An instance of this class stores it's own state. Use getFilter to extract
-// the complete state of the filter so far.
-//
-// Use the run function to run and store the report in the api.
-
-// Future: consider restructuring this class so that it's methods which mutate
-// state operate on and return a new react state object.
+/**
+ * Gradually build a filter for a report run with help from the api.
+ *
+ * Use the getHints function where available on fields to get sample
+ * field values.
+ *
+ * Use setFilter to set simple filter values.
+ *
+ * Use the multifilter functions for filter values which are a collection of
+ * items.
+ *
+ * An instance of this class stores it's own state. Use getFilter to extract
+ * the complete state of the filter so far.
+ *
+ * Use the run function to run and store the report in the api.
+ *
+ * name: Initial name of the report.
+ * rpId: Report Presentation id. // TODO: switch to report type data type
+ * filters: List of filters supported by this report.
+ * api: Reporting API instance to use.
+ * onNewReport: call back when a new report has been created.
+ * onNameChanged: call back when the report name changes.
+ * onUpdateFilter: call back with the filter changes.
+ * onErrorsChanged: call back when the list of user errors changes.
+ */
 export class ReportConfiguration {
   constructor(name, rpId, filters, api,
       onNewReport, onNameChanged, onUpdateFilter, onErrorsChanged) {
@@ -100,6 +133,9 @@ export class ReportConfiguration {
     this.onErrorsChanged = onErrorsChanged;
   }
 
+  /**
+   * Return a collection of hints for a particular field and partial input.
+   */
   async getHints(field, partial) {
     return await this.api.getHelp(
       this.rpId, this.getFilter(), field, partial);
@@ -111,6 +147,9 @@ export class ReportConfiguration {
     this.onUpdateFilter(filterInfo);
   }
 
+  /**
+   * Set a value for a single valued filter.
+   */
   setFilter(field, value) {
     if (value === undefined || value === null || value === '') {
       delete this.simpleFilter[field];
@@ -120,6 +159,9 @@ export class ReportConfiguration {
     this.callUpdateFilter();
   }
 
+  /**
+   * Set a value for a multiple valued "or" filter.
+   */
   addToMultifilter(field, obj) {
     if (!(field in this.multiFilter)) {
       this.multiFilter[field] = [];
@@ -130,11 +172,18 @@ export class ReportConfiguration {
     this.callUpdateFilter();
   }
 
+
+  /**
+   * Get a previously set value for a multiple valued "or" filter.
+   */
   removeFromMultifilter(field, obj) {
     remove(this.multiFilter[field], i => i.key === obj.key);
     this.callUpdateFilter();
   }
 
+  /**
+   * Add a value to a "and/or" filter. i.e. tags
+   */
   addToAndOrFilter(field, index, obj) {
     if (!(field in this.andOrFilter)) {
       this.andOrFilter[field] = [];
@@ -146,6 +195,9 @@ export class ReportConfiguration {
     this.callUpdateFilter();
   }
 
+  /**
+   * Remove a value to a "and/or" filter. i.e. tags
+   */
   removeFromAndOrFilter(field, index, obj) {
     const found = this.andOrFilter[field][index].findIndex(i => i.key === obj.key);
     if (found !== -1) {
@@ -157,6 +209,9 @@ export class ReportConfiguration {
     this.callUpdateFilter();
   }
 
+  /**
+   * Build a filter suitable for sending to the run method of the api.
+   */
   getFilter() {
     const filter = {...this.simpleFilter};
     for (const key in this.multiFilter) {
@@ -173,17 +228,28 @@ export class ReportConfiguration {
     return filter;
   }
 
+  /**
+   * Change the name of the report.
+   */
   changeReportName(name) {
     this.name = name;
     this.onNameChanged(name);
   }
 
+  /**
+   * Run callbacks to get all current stae.
+   *
+   * Useful at component initialization time.
+   */
   runCallbacks() {
     this.onErrorsChanged(this.errors);
     this.onNameChanged(this.name);
     this.callUpdateFilter();
   }
 
+  /**
+   * Run the report.
+   */
   async run() {
     try {
       const response = await this.api.runReport(
@@ -203,9 +269,11 @@ export class ReportConfiguration {
   }
 }
 
-// This factory just builds a ReportConfiguration.
-//
-// Having it makes unit testing easier.
+/**
+ * This factory just builds a ReportConfiguration.
+ *
+ * Having this makes unit testing easier.
+ */
 export class ReportConfigurationBuilder {
   constructor(api) {
     this.api = api;
