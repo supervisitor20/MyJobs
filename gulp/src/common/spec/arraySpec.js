eb4387abfe5d231ad64c6b56bda9c2c8a86064a2
Darrin Thompson <dthompson@apps.directemployers.org>
2016-04-08 11:22:07 -0400

    FlatMap is redundant with lodash and unused.

--- gulp/src/common/spec/arraySpec.js
+++ gulp/src/common/spec/arraySpec.js
@@ -1,4 +1,4 @@
-import {intersperse, flatMap, lookupByValue} from '../array';
+import {intersperse, lookupByValue} from '../array';
 
 
 const add200 = (item, index) => item + index + 200;
@@ -16,23 +16,6 @@ describe('intersperse', () => {
     const add300 = (item, index) => item + index + 300;
     expect(intersperse(add300, add200, [1, 3, 7]))
       .toEqual([201, 304, 205, 310, 211]);
-  });
-});
-
-const add200Twice = (item, index) => [item + 200, index + 200];
-
-describe('flatMap', () => {
-  it('can handle an empty input', () => {
-    expect(flatMap(null, [])).toEqual([]);
-  });
-
-  it('can handle one element', () => {
-    expect(flatMap(add200Twice, [1000])).toEqual([1200, 200]);
-  });
-
-  it('can handle multiple elements', () => {
-    expect(flatMap(add200Twice, [1000, 1001]))
-      .toEqual([1200, 200, 1201, 201]);
   });
 });
 
