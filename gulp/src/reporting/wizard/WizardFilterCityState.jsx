6af9776e82d8d84cc6af359664193caa0ba90661
aspidites <emarshall85@gmail.com>
2016-04-22 08:57:41 -0400

    Modified shape of state to allow for additions later on.

--- gulp/src/reporting/wizard/WizardFilterCityState.jsx
+++ gulp/src/reporting/wizard/WizardFilterCityState.jsx
@@ -5,21 +5,24 @@ import {SearchInput} from 'common/ui/SearchInput';
 export class WizardFilterCityState extends Component {
     constructor() {
       super();
-      this.state = {city: '', state: ''};
+      this.state = {
+        currentLocation: {
+          city: '',
+          state: '',
+        },
+      };
     }
 
     updateField(field, value) {
       const {updateFilter} = this.props;
 
       // Update parent
-      const newFilter = {...this.state};
-      newFilter[field] = value.key;
-      updateFilter(newFilter);
+      const {currentLocation} = this.state;
+      currentLocation[field] = value.key;
+      updateFilter(currentLocation);
 
       // Set internal state
-      const newState = {};
-      newState[field] = value.key;
-      this.setState(newState);
+      this.setState(...this.state, currentLocation);
     }
 
     render() {
