e3972c2d7e683385563fe26968a385f8f496bc99
Jon Koons <jkoons@apps.directemployers.org>
2016-04-26 11:04:29 -0400

    add test data, adjust table

--- gulp/src/nonuseroutreach/components/OutreachRecordTable.jsx
+++ gulp/src/nonuseroutreach/components/OutreachRecordTable.jsx
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
-import {Table} from 'fixed-data-table';
+import {Table, Column, Cell} from 'fixed-data-table';
 
 export class OutreachRecordTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [
+        {name: 'Fred'},
+        {name: 'George'},
+        {name: 'Mary'},
+      ],
+    };
   }
 
   // ajax call?
 
   render() {
     return (
-      <div></div>
+      <Table
+        rowsCount={this.state.data.length}
+        rowHeight={50}
+        headerHeight={50}
+        width={200}
+        maxHeight={400}
+        {...this.props}>
+        <Column
+          header={<Cell>Name</Cell>}
+          cell={props => (
+            <Cell {...props}>
+              {this.state.data[props.rowIndex].name}
+            </Cell>
+            )}
+          width={200}
+        />
+      </Table>
     );
   }
 }
