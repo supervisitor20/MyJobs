59dd919c6a78c7095756f97fc018524e7965c4c2
Jon Koons <jkoons@apps.directemployers.org>
2016-04-26 14:47:54 -0400

    add responsive code for table sizing

--- gulp/src/nonuseroutreach/components/OutreachRecordTable.jsx
+++ gulp/src/nonuseroutreach/components/OutreachRecordTable.jsx
@@ -5,7 +5,9 @@ export class OutreachRecordTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
+      tableHeight: 0,
+      tableWidth: 0,
+      records: [
         {name: 'Fred'},
         {name: 'George'},
         {name: 'Mary'},
@@ -14,21 +16,48 @@ export class OutreachRecordTable extends Component {
   }
 
   // ajax call?
+  componentDidMount() {
+    this._update();
+    const win = window;
+    if (win.addEventListener) {
+      win.addEventListener('resize', this._onResize, false);
+    } else if (win.attachEvent) {
+      win.attachEvent('onresize', this._onResize);
+    } else {
+      win.onresize = this._onResize;
+    }
+  }
+
+  _onResize() {
+    clearTimeout(this._updateTimer);
+    this._updateTimer = setTimeout(this._update, 16);
+  }
+
+  _update() {
+    const win = window;
+
+    const widthOffset = win.innerWidth < 680 ? 0 : 500;
+
+    this.setState({
+      tableWidth: win.innerWidth - widthOffset,
+      tableHeight: win.innerHeight - 200,
+    });
+  }
 
   render() {
     return (
       <Table
-        rowsCount={this.state.data.length}
+        rowsCount={this.state.records.length}
         rowHeight={50}
         headerHeight={50}
-        width={200}
-        maxHeight={400}
+        width={this.state.tableWidth}
+        height={this.state.tableHeight}
         {...this.props}>
         <Column
           header={<Cell>Name</Cell>}
           cell={props => (
             <Cell {...props}>
-              {this.state.data[props.rowIndex].name}
+              {this.state.records[props.rowIndex].name}
             </Cell>
             )}
           width={200}
