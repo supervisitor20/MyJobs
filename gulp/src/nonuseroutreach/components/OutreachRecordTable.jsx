3208a227247fae24d4dc9c386fe0084062974cd0
Jon Koons <jkoons@apps.directemployers.org>
2016-04-29 13:58:08 -0400

    use react bootstrap in lieu of fixed data table

--- gulp/src/nonuseroutreach/components/OutreachRecordTable.jsx
+++ gulp/src/nonuseroutreach/components/OutreachRecordTable.jsx
@@ -1,125 +1,41 @@
 import React, {Component} from 'react';
-import {Table, Column, Cell} from 'fixed-data-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import _ from 'lodash-compat';
 
 export class OutreachRecordTable extends Component {
+
+  // It's a data format example.
+  //priceFormatter(cell, row){
+  //  return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+  //}
+  //dataFormat={this.priceFormatter}
+
   constructor(props) {
     super(props);
     this.state = {
-      tableHeight: 0,
-      tableWidth: 0,
-      records: [
-        {
-          date: '01/01/2016',
-          inbox: 'testinbox@test.com',
-          from: 'outreachdude@test.com',
-          body: 'Reach out to a dude that...',
-          state: 'Reviewed'
-        },
-        {
-          date: '01/01/2016',
-          inbox: 'testinbox@test.com',
-          from: 'outreachdude@test.com',
-          body: 'Reach out to a dude that...',
-          state: 'Reviewed'
-        },
-        {
-          date: '01/01/2016',
-          inbox: 'testinbox@test.com',
-          from: 'outreachdude@test.com',
-          body: 'Reach out to a dude that...',
-          state: 'Reviewed'
-        },
-        {
-          date: '01/01/2016',
-          inbox: 'testinbox@test.com',
-          from: 'outreachdude@test.com',
-          body: 'Reach out to a dude that...',
-          state: 'Reviewed'
-        },
-      ],
-    };
-  }
-
-  // ajax call?
-  componentDidMount() {
-    this._update();
-    const win = window;
-    if (win.addEventListener) {
-      win.addEventListener('resize', _.debounce(() => this._update(), 16), false);
-    } else if (win.attachEvent) {
-      win.attachEvent('onresize', _.debounce(() => this._update(), 16));
-    } else {
-      win.onresize = () => _.debounce(this._update(), 16);
+      products: [{
+        id: 1,
+        name: "Item name 1",
+        price: 100
+        },{
+        id: 2,
+        name: "Item name 2",
+        price: 100
+      }]
     }
-  }
-
-  _update() {
-    const win = window;
-    const widthOffset = win.innerWidth > 980 ? win.innerWidth / 2.5 : win.innerWidth / 3
-    console.log("update called");
-
-    this.setState({
-      tableWidth: win.innerWidth - widthOffset,
-      tableHeight: win.innerHeight - 200,
-    });
   }
 
   render() {
     return (
-      <Table
-        rowsCount={this.state.records.length}
-        rowHeight={50}
-        headerHeight={50}
-        width={this.state.tableWidth}
-        height={this.state.tableHeight}
-        {...this.props}>
-        <Column
-          header={<Cell>Date</Cell>}
-          cell={props => (
-            <Cell {...props}>
-              {this.state.records[props.rowIndex].date}
-            </Cell>
-            )}
-          width={90}
-        />
-        <Column
-          header={<Cell>Inbox</Cell>}
-          cell={props => (
-            <Cell {...props}>
-              {this.state.records[props.rowIndex].inbox}
-            </Cell>
-            )}
-          width={200}
-        />
-        <Column
-          header={<Cell>From</Cell>}
-          cell={props => (
-            <Cell {...props}>
-              {this.state.records[props.rowIndex].from}
-            </Cell>
-            )}
-          width={200}
-        />
-        <Column
-          header={<Cell>Body</Cell>}
-          cell={props => (
-            <Cell {...props}>
-              {this.state.records[props.rowIndex].body}
-            </Cell>
-            )}
-          width={200}
-        />
-        <Column
-          header={<Cell>Action State</Cell>}
-          cell={props => (
-            <Cell {...props}>
-              {this.state.records[props.rowIndex].state}
-            </Cell>
-            )}
-          width={150}
-        />
-      </Table>
+      <BootstrapTable data={this.state.products} striped={false} hover={true}
+                      pagination={true} search={true}>
+        <TableHeaderColumn dataField="id" isKey={true} dataAlign="center"
+                           dataSort={true}>Product ID</TableHeaderColumn>
+        <TableHeaderColumn dataField="name" dataSort={true}>Product
+          Name</TableHeaderColumn>
+        <TableHeaderColumn dataField="price" >Product
+          Price</TableHeaderColumn>
+      </BootstrapTable>
     );
   }
 }
