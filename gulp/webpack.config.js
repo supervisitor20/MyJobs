bda3ea7b6d387e81908e7416dfa2222afa073148
aspidites <emarshall85@gmail.com>
2016-05-18 08:58:08 -0400

    Made things slightly less configurable.

--- gulp/webpack.config.js
+++ gulp/webpack.config.js
@@ -3,10 +3,6 @@ var path = require('path');
 
 module.exports = {
   devServer: {
-    hot: true,
-    progress: true,
-    inline: true,
-    stats: 'errors-only',
     host: process.env.DEVSERVER_HOST || "0.0.0.0",
     port: process.env.DEVSERVER_PORT || "8080",
     config: "webpack.dev.config.js",
