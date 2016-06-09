ad3b914de4ef6b03b49d1a5a501698c0282c67a0
aspidites <emarshall85@gmail.com>
2016-06-09 09:32:20 -0400

    Started breaking things on purpose.

--- gulp/src/common/create-redux-store.js
+++ gulp/src/common/create-redux-store.js
@@ -3,29 +3,17 @@ import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise';
 
 
-function useDevTools() {
-  if (process.env.NODE_ENV === 'production') {
-    // Not for production
-    return false;
-  } else if (typeof window === 'undefined') {
-    // Not for unit tests
-    return false;
-  } else if (!window.devToolsExtension) {
-    // Not if dev tools aren't present.
-    return false;
-  }
-  return true;
-}
+const useDevTools = process.env.NODE_ENV !== 'production' &&
+  typeof window !== 'undefined' && !!window.devToolsExtension;
 
-
-const devToolsMiddleware = useDevTools() ? window.devToolsExtension() : f => f;
+const devToolsMiddleware = useDevTools ? window.devToolsExtension() : f => f;
 
 
 /**
  * Create redux store with our standard middlewares.
  *
  * reducer: reducer for the store
- * defaultState: optional default state for the reducer. otherwise undefined
+ * defaultState: optional default state for the reducerk
  * thunkExtra: extra arg for thunk middleware.
  */
 export default function createReduxStore(reducer, defaultState, thunkExtra) {
--- gulp/src/nonuseroutreach/main.jsx
+++ gulp/src/nonuseroutreach/main.jsx
@@ -4,18 +4,20 @@ import {MyJobsApi} from '../common/myjobs-api';
 import Api from './api';
 import {getCsrf} from 'common/cookie';
 import {Container} from'./components/Container';
-import {InboxManagement, OutreachRecordManagement} from './nonuseroutreachEngine';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// new imports
+import createReduxStore from '../common/create-redux-store';
+import {combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 
 installPolyfills();
 
 const myJobsApi = new MyJobsApi(getCsrf());
 const nuoApi = new Api(myJobsApi);
-const inboxManager = new InboxManagement(nuoApi);
-const recordsManager = new OutreachRecordManagement(nuoApi);
 
 ReactDOM.render(
-  <Container inboxManager = {inboxManager} recordsManager = {recordsManager} />,
-    document.getElementById('content')
+  <Container api = {nuoApi} />,
+  document.getElementById('content')
 );
