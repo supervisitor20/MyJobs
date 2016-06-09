0fa90ef647d94ac33f47b316dfa4d38fc74a0adb
aspidites <emarshall85@gmail.com>
2016-06-09 14:12:14 -0400

    Reducing granularity of components.

--- gulp/src/nonuseroutreach/components/Menu.jsx
+++ gulp/src/nonuseroutreach/components/Menu.jsx
@@ -1,31 +1,46 @@
 import React, {PropTypes} from 'react';
-
-import {InboxManagementButton} from './InboxManagementButton';
-import {OverviewButton} from './OverviewButton';
-import {OutreachRecordButton} from './OutreachRecordButton';
+import Button from 'react-bootstrap/lib/Button';
 
 // navigation links
-export function Menu(props) {
-  const {tips, changePage} = props;
-  let tipsHeader;
-  let tipsComponent;
-  if (tips && tips.length > 0) {
-    tipsComponent = tips.map((tip, i) => <p key={i}>{tip}</p>
-    );
-    tipsHeader = <h2>Tips</h2>;
-  }
-  return (
-    <div className="col-xs-12 col-md-4">
-      <div className="sidebar">
-        <h2 className="top">Navigation</h2>
-        <OverviewButton changePage={changePage} />
-        <InboxManagementButton changePage={changePage} />
-        <OutreachRecordButton changePage={changePage} />
-        {tipsHeader}
-        {tipsComponent}
+export class Menu extends React.Component {
+  render() {
+    const {tips, changePage} = this.props;
+    const inboxTips = [
+      'Use this page to manage the various email addresses to which ' +
+      'you will have your employees send outreach emails',
+    ];
+    const outreachTips = [
+      'Use this page to view outreach records from non-users',
+    ];
+
+    let tipsHeader;
+    let tipsComponent;
+    if (tips && tips.length > 0) {
+      tipsComponent = tips.map((tip, i) => <p key={i}>{tip}</p>
+      );
+      tipsHeader = <h2>Tips</h2>;
+    }
+
+    return (
+
+      <div className="col-xs-12 col-md-4">
+        <div className="sidebar">
+          <h2 className="top">Navigation</h2>
+          <Button onClick={() => changePage('Overview')}>
+            Overview
+          </Button>
+          <Button onClick={() => changePage('InboxManagement', inboxTips)}>
+            Inbox Management
+          </Button>
+          <Button onClick={() => changePage('OutreachRecords', outreachTips)}>
+            Outreach Records
+          </Button>
+          {tipsHeader}
+          {tipsComponent}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Menu.propTypes = {
