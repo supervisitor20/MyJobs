07f22209f9796dbb1a888ade9f889cf0a083929a
aspidites <emarshall85@gmail.com>
2016-06-29 08:33:03 -0400

    Started fleshing out styles.

--- gulp/src/importwizard/components/ImportOptionsPage.jsx
+++ gulp/src/importwizard/components/ImportOptionsPage.jsx
@@ -5,6 +5,17 @@ import {MyJobsApi} from 'common/myjobs-api';
 import {getCsrf} from 'common/cookie';
 
 const api = new MyJobsApi(getCsrf());
+const excelMimeType =
+  'application/vnd.ms-excel,' +
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+
+const style = {
+  width: '100%',
+  height: '50px',
+  borderColor: 'rgb(102, 102, 102)',
+  borderStyle: 'dashed',
+  borerRadius: '5px',
+};
 
 export default class ImportOptionsPage extends React.Component {
   async onDrop(files) {
@@ -16,9 +27,20 @@ export default class ImportOptionsPage extends React.Component {
     return (
       <Row>
         <Col xs={12}>
-          <Dropzone onDrop={files => this.onDrop(files)} multiple={false}>
-          Try dragging a spreadsheet here or clicking to select one.
-          </Dropzone>
+          <Row>
+            <Col md={4}>
+              Import File
+            </Col>
+            <Col md={8}>
+              <Dropzone
+                style={style}
+                accept={excelMimeType}
+                onDrop={files => this.onDrop(files)}
+                multiple={false}>
+                Try dragging a spreadsheet here or clicking to select one.
+              </Dropzone>
+            </Col>
+          </Row>
         </Col>
       </Row>
     );
