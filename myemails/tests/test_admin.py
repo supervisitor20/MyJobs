d1a55a8de1b8e79f6357e52b356e716248e19614
Matt Lauber <github@mklauber.com>
2016-07-12 16:02:25 -0400

    More test changes.

--- myemails/tests/test_admin.py
+++ myemails/tests/test_admin.py
@@ -5,6 +5,6 @@ class TestAdminLoginForm(MyJobsBase):
     def test_admin_login_form_has_autocomplete_off(self):
         form = MyJobsAdminAuthenticationForm()
         attributes = form.fields['password'].widget.attrs
-        self.assertDictContainsSubset(attributes, {'autocomlete': 'off'}, 
+        self.assertDictContainsSubset({'autocomlete': 'off'}, attributes,  
             "The widget of the password field of the admin login form should "\
             "have the attribute autocomplete set to 'off'.")
