2db51784143ddefe67abd9a121967b308a5ccbc9
Matt Lauber <github@mklauber.com>
2016-07-12 14:12:21 -0400

    Add a test asserting that the attribute is on the form.

---
+++ myemails/tests/test_admin.py
@@ -0,0 +1,10 @@
+from myjobs.tests.setup import MyJobsBase
+from myjobs.forms import MyJobsAdminAuthenticationForm
+class TestAdminLoginForm(MyJobsBase):
+    
+    def test_admin_login_form_has_autocomplete_off(self):
+        form = MyJobsAdminAuthenticationForm()
+        attributes = form.password.widget.attrs
+        self.assertDictContainsSubset(attributes, {'autocomlete': 'off'}, 
+            "The widget of the password field of the admin login form should "\
+            "have the attribute autocomplete set to 'off'.")
