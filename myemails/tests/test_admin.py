de75872fddbb78c58c6cfbf75bba0a0931b31f25
Matt Lauber <github@mklauber.com>
2016-07-13 08:29:47 -0400

    typo, plus better errors.

--- myemails/tests/test_admin.py
+++ myemails/tests/test_admin.py
@@ -5,6 +5,7 @@ class TestAdminLoginForm(MyJobsBase):
     def test_admin_login_form_has_autocomplete_off(self):
         form = MyJobsAdminAuthenticationForm()
         attributes = form.fields['password'].widget.attrs
-        self.assertDictContainsSubset({'autocomlete': 'off'}, attributes,  
+        self.assertDictContainsSubset({'autocomplete': 'off'}, attributes,  
             "The widget of the password field of the admin login form should "\
-            "have the attribute autocomplete set to 'off'.")
+            "have the attribute autocomplete set to 'off'.  It's value \n%s\n"\
+            "does not contain {'autocomplete':'off'}" % attributes)
