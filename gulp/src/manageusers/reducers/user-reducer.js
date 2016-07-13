0982b76262739b18a92a5c1d4b566823bd441515
aspidites <emarshall85@gmail.com>
2016-07-13 12:10:33 -0400

    Fixed reducer.

--- gulp/src/manageusers/reducers/user-reducer.js
+++ gulp/src/manageusers/reducers/user-reducer.js
@@ -3,5 +3,5 @@ import {handleActions} from 'redux-actions';
 export const initialUsers = {};
 
 export default handleActions({
-  'UPDATE_USERS': (state, action) => ({...state, ...action.payload}),
+  'UPDATE_USERS': (state, action) => action.payload,
 }, initialUsers);
