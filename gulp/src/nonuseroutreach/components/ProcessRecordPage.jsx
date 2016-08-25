43e8d2511a09ca5d52a5096e4264827c2602161e
huy-nguyen-colorado <huy.nguyen.colorado.github@gmail.com>
2016-08-25 14:50:17 -0400

    PD-2575, added missing history proptypes

--- gulp/src/nonuseroutreach/components/ProcessRecordPage.jsx
+++ gulp/src/nonuseroutreach/components/ProcessRecordPage.jsx
@@ -73,7 +73,7 @@ class ProcessRecordPage extends Component {
   }
 
   async handleSubmit() {
-    const {dispatch} = this.props;
+    const {dispatch, history} = this.props;
 
     await dispatch(doSubmit(false, () => history.pushState(null, '/records')));
   }
@@ -236,6 +236,7 @@ class ProcessRecordPage extends Component {
 
 ProcessRecordPage.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
   outreachId: PropTypes.string.isRequired,
   processState: PropTypes.string.isRequired,
   partnerName: PropTypes.string,
