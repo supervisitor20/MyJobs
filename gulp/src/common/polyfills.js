0ced7b766aac791d2e9b80fb9d254baecd03dbb5
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-09-09 11:48:50 -0400

    pd-2619-Cannot-open-records-in-ie8 Adding the eventlistener script to the polyfill.js making it global

--- gulp/src/common/polyfills.js
+++ gulp/src/common/polyfills.js
@@ -27,4 +27,39 @@ export function installPolyfills() {
   if (!Date.now) {
     Date.now = () => new Date().getTime();
   }
+
+  // IE8 doesn't support Add & Remove event listener
+  /*
+    The original code for this Event Listener Pollyfill can be found here: https://gist.github.com/jonathantneal/3748027
+  */
+  if ( !window.addEventListener) {
+    (function fixIE8(WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
+      WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function prototype(type, listener) {
+        const target = this;
+
+        registry.unshift([target, type, listener, (event) => {
+          event.currentTarget = target;
+          event.preventDefault = function preventDefault() { event.returnValue = false; };
+          event.stopPropagation = function stopPropagation() { event.cancelBubble = true; };
+          event.target = event.srcElement || target;
+
+          listener.call(target, event);
+        }]);
+
+        this.attachEvent('on' + type, registry[0][3]);
+      };
+
+      WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function eventListener(type, listener) {
+        for (let index = 0, register; register === registry[index]; ++index) {
+          if (register[0] === this && register[1] === type && register[2] === listener) {
+            return this.detachEvent('on' + type, registry.splice(index, 1)[0][3]);
+          }
+        }
+      };
+
+      WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function dispatchEventIE8(eventObject) {
+        return this.fireEvent('on' + eventObject.type, eventObject);
+      };
+    })(Window.prototype, HTMLDocument.prototype, Element.prototype, 'addEventListener', 'removeEventListener', 'dispatchEvent', []);
+  }
 }
