27ec4f6c98353ee4ae5881f134cd4856e9ee23f7
Jon Koons <jkoons@apps.directemployers.org>
2016-10-17 14:14:30 -0400

    testing mongo queries

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, patterns
 
 # API URLs
 urlpatterns = patterns('analytics.api',
-   url(r'^views-by-date', 'views_by_date', name='views_by_date'),
+   url(r'^views-week', 'views_last_7_days', name='views_last_7_days'),
 )
