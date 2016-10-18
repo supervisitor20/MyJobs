eb023ce195e9c4c9ea25c0ecf795ddf95e24a517
Jon Koons <jkoons@apps.directemployers.org>
2016-10-18 14:31:39 -0400

    checkpoint.. includes all tested mongo queries

--- analytics/migrations/0001_add_activities_roles.py
+++ analytics/migrations/0001_add_activities_roles.py
@@ -4,6 +4,8 @@ from south.db import db
 from south.v2 import DataMigration
 from django.db import models
 
+from myjobs.models import Activity, AppAccess
+
 class Migration(DataMigration):
 
     def forwards(self, orm):
@@ -13,8 +15,8 @@ class Migration(DataMigration):
 
         """
 
-        analytics = orm['myjobs.AppAccess'].objects.create(name="Analytics")
-        orm['myjobs.Activity'].objects.create(
+        analytics = AppAccess.objects.create(name="Analytics")
+        Activity.objects.create(
             name="view analytics",
             description="Access Analytics Graphs and Reporting.",
             app_access=analytics
@@ -26,8 +28,8 @@ class Migration(DataMigration):
 
 
         """
-        analytics = orm['myjobs.AppAccess'].objects.get(name="Analytics")
-        orm['myjobs.Activity'].objects.filter(app_access=analytics).delete()
+        analytics = AppAccess.objects.get(name="Analytics")
+        Activity.objects.filter(app_access=analytics).delete()
         analytics.delete()
 
     models = {}
