0af49f904bc03057712b677c246b08840225118d
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-10-19 10:51:26 -0400

    Editing more of the charts to be shown

--- gulp/src/analytics/main.jsx
+++ gulp/src/analytics/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import AreaChart from './components/AreaChart';
 import BarChart from './components/BarChart';
 import PieChart from './components/PieChart';
+import RadialBarChart from './components/RadialBarChart';
+import ComposedChart from './components/ComposedChart';
 
 ReactDOM.render(
   <div>
     <AreaChart/>
     <BarChart/>
     <PieChart/>
+    <RadialBarChart/>
+    <ComposedChart/>
   </div>
   , document.getElementById('content')
 );
