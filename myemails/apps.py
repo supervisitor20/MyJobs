1d88ff823fbec96cef309c988e284bdd0ef6890b
Matt Lauber <github@mklauber.com>
2016-11-07 14:51:27 -0500

    A better way of handling signals, I think.

--- myemails/apps.py
+++ myemails/apps.py
@@ -9,11 +9,9 @@ class MyEmailsConfig(AppConfig):
         Event = self.get_model("Event")
         EmailTask = self.get_model("EmailTask")
 
-        def event_receiver(signal, content_type, type_):
+        def event_receiver(signal, sender, type_):
             def event_receiver_decorator(fn):
-                from django.contrib.auth.models import ContentType
-                sender = ContentType.objects.get(model=content_type).model_class()
-                dispatch_uid = '%s_%s_%s' % (fn.__name__, content_type, type_)
+                dispatch_uid = '%s_%s_%s' % (fn.__name__, sender, type_)
                 wrapper = receiver(signal, sender=sender, dispatch_uid=dispatch_uid)
                 return wrapper(fn)
 
@@ -23,8 +21,8 @@ class MyEmailsConfig(AppConfig):
         # attribute, get again post save), but we need to be able to 1) determine what
         # was changed and 2) only send an email if the save is successful. - TP
 
-        @event_receiver(pre_save, 'invoice', 'created')
-        @event_receiver(pre_save, 'purchasedproduct', 'created')
+        @event_receiver(pre_save, 'postajob.invoice', 'created')
+        @event_receiver(pre_save, 'postajob.purchasedproduct', 'created')
         def pre_add_invoice(sender, instance, **kwargs):
             """
             Determines if an invoice has been added to the provided instance.
@@ -39,8 +37,8 @@ class MyEmailsConfig(AppConfig):
                 invoice_added = not hasattr(original, 'invoice') and invoice_added
             instance.invoice_added = invoice_added
 
-        @event_receiver(post_save, 'invoice', 'created')
-        @event_receiver(post_save, 'purchasedproduct', 'created')
+        @event_receiver(post_save, 'postajob.invoice', 'created')
+        @event_receiver(post_save, 'postajob.purchasedproduct', 'created')
         def post_add_invoice(sender, instance, **kwargs):
             """
             Schedules tasks for the instance if pre_add_invoice determined that an
