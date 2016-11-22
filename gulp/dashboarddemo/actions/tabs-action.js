beb045c2ef7a3b716bacb1f574c848ca5dd96ed2
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-11-22 14:49:18 -0500

    Deleting the rest of the files for tracking

--- gulp/dashboarddemo/actions/tabs-action.js
+++
@@ -1,9 +0,0 @@
-export function loadTabs(){
-  return function(dispatch){
-    return DataApi.getAllData().then(data => {
-      dispatch("ACTION CREATOR GOES HERE");
-    }).catch(error => {
-      throw(error);
-    });
-  }
-}
