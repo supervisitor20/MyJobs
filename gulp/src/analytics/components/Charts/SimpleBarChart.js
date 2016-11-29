fec957eb01da0045c0616927f47569481c6e2a81
Darrin Thompson <dthompson@apps.directemployers.org>
2016-11-29 15:20:07 -0500

    Make chart work with react-d3.

--- gulp/src/analytics/components/Charts/SimpleBarChart.js
+++ gulp/src/analytics/components/Charts/SimpleBarChart.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import {Component} from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+import {
+  BarChart,
+} from 'react-d3-basic';
 
 class SimpleBarChart extends Component {
-	render() {
-  const {chartData} = this.props;
-  const axisData = chartData.PageLoadData.rows;
-  axisData.map((obj) => {
-    obj.job_views = parseInt(obj.job_views, 10);
-  });
-  const colors = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  return (
-			<div id="bar_chart" style={{width: '100%', maxHeight: '400px', height: '400px'}}>
-				<ResponsiveContainer>
-					<BarChart width={1200} height={400} data={axisData}>
-						<XAxis dataKey="browser"/>
-						<YAxis/>
-						<CartesianGrid strokeDasharray="3 3"/>
-						<Tooltip/>
-						<Legend />
-						<Bar dataKey="job_views" fill={colors} />
-					</BarChart>
-				</ResponsiveContainer>
-			</div>
-		);
-	}
+    render() {
+      const {chartData} = this.props;
+      const axisData = chartData.PageLoadData.rows;
+      const chartSeries = [
+        {
+          field: 'job_views',
+          name: 'Job Views',
+        },
+      ];
+      const x = d => d.browser;
+      return (
+        <div id="bar_chart" style={{
+          width: '100%',
+          maxHeight: '400px',
+          height: '400px'}}>
+          <BarChart
+            width={1200}
+            height={400}
+            chartSeries={chartSeries}
+            x = {x}
+            data={axisData}
+            xScale="ordinal"
+            xLabel="Browser" />
+        </div>
+      );
+    }
 }
 
 SimpleBarChart.propTypes = {
