459361dcb721e47502327270563f067b755e1409
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-12-15 16:52:18 -0500

    Editing the header calendar to take out of this PR

--- gulp/src/analytics/components/Header/Header.js
+++ gulp/src/analytics/components/Header/Header.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import {Component} from 'react';
 import {connect} from 'react-redux';
-import DateField from 'common/ui/DateField';
-import CalendarPanel from 'common/ui/CalendarPanel';
 
 class Header extends Component {
-  logIt() {
-    console.log('HELLO WORLD');
-  }
   render() {
     return (
       <div className="tabs-header">
@@ -19,29 +14,6 @@ class Header extends Component {
             <li><a href="#"><span className="head-icon fa fa-file-excel-o"></span></a></li>
           </ul>
         </nav>
-        <DateField
-          name='Calendar'
-          onChange={e => this.logIt()}
-          required={true}
-          value='25'
-          maxLength={7}
-          isHidden={false}
-          placeholder='Calendar'
-          autoFocus='Date'
-          numberOfYears={50}
-          disable={false}
-          pastOnly
-          />
-        <div style={{marginBottom: '200px'}}>
-        <CalendarPanel
-            year={2016}
-            yearChoices={[2015, 2016]}
-            month={12}
-            day={15}
-            onSelect={e => console.log('On Select', e)}
-            onYearChange={e => console.log('On Year', e)}
-          />
-        </div>
       </div>
     );
   }
