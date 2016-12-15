f8932de7d8f939fb7d0b7fd4f7fb933557dccaa1
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-12-15 12:20:00 -0500

    Cleaning up the package.json folder and the node modules getting rid of unnecessary packages along with implementing a few of the charts that we will use

--- gulp/src/analytics/components/Charts/ChartContainer.js
+++ gulp/src/analytics/components/Charts/ChartContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Component} from 'react';
 import {Row, Col} from 'react-bootstrap';
-// import BarChart from './Bar/BarChart';
-import WorldMap from './Map/WorldMap';
-import USAMap from './Map/USAMap';
+import SimpleBarChart from './Bar/BarChart';
+// import SimpleLineChart from './Line/LineChart';
+// import SimplePieChart from './Pie/PieChart';
+// import WorldMap from './Map/WorldMap';
+// import USAMap from './Map/USAMap';
 
 class ChartContainer extends Component {
   constructor(props, context) {
@@ -11,18 +13,18 @@ class ChartContainer extends Component {
   }
   render() {
     const {chartData} = this.props;
-    const chartDisplay = chartData.PageLoadData.column_names[0].key;
-    let chartType;
-    switch (chartDisplay) {
-    case 'country':
-      chartType = <WorldMap width={1920} height={800} chartData={chartData} />;
-      break;
-    case 'state':
-      chartType = <USAMap width={1920} height={700} chartData={chartData} />;
-      break;
-    default:
-      chartType = <WorldMap chartData={chartData} />;
-    }
+    // const chartDisplay = chartData.PageLoadData.column_names[0].key;
+    // let chartType;
+    // switch (chartDisplay) {
+    // case 'country':
+    //   chartType = <WorldMap width={1920} height={800} chartData={chartData} />;
+    //   break;
+    // case 'state':
+    //   chartType = <USAMap width={1920} height={700} chartData={chartData} />;
+    //   break;
+    // default:
+    //   chartType = <WorldMap chartData={chartData} />;
+    // }
     return (
         <div id={'chart_tab_' + chartData.navId} className="charts">
           <Row>
@@ -35,7 +37,7 @@ class ChartContainer extends Component {
           <hr/>
             <Row>
               <Col md={12}>
-                {chartType}
+                <SimpleBarChart/>
               </Col>
             </Row>
         </div>
--- gulp/src/analytics/components/Charts/Line/LineChart.js
+++ gulp/src/analytics/components/Charts/Line/LineChart.js
@@ -1,123 +1,35 @@
-import d3 from 'd3';
 import React from 'react';
-import Axis from '../Common/Axis';
-import Grid from '../Common/Grid';
+import {Component} from 'react';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'Recharts';
 
-const LineChart = React.createClass({
-  getDefaultProps(){
-    return {
-      svgHeight: 600,
-      svgWidth: 1920,
-      svgId: 'line_svg'
-    };
-  },
-  render(){
-    const margin = { top: 20, bottom: 100, left: 80, right: 20 },
-          height = this.props.svgHeight - margin.top - margin.bottom,
-          width = this.props.svgWidth - margin.left - margin.right,
-          transform = 'translate(' + margin.left + ',' + margin.top + ')',
-          interpolations = [
-            "linear",
-            "step-before",
-            "step-after",
-            "basis",
-            "basis-closed",
-            "cardinal",
-            "cardinal-closed"
-          ],
-          data = [
-            {"day":"2016/10/10", "hits": 476},
-            {"day":"2016/10/11", "hits": 678},
-            {"day":"2016/10/12", "hits": 567},
-            {"day":"2016/10/13", "hits": 425},
-            {"day":"2016/10/14", "hits": 387},
-            {"day":"2016/10/15", "hits": 1025},
-            {"day":"2016/10/16", "hits": 978},
-            {"day":"2016/10/17", "hits": 376},
-            {"day":"2016/10/18", "hits": 267},
-            {"day":"2016/10/19", "hits": 1167},
-            {"day":"2016/10/20", "hits": 729},
-            {"day":"2016/10/21", "hits": 644},
-            {"day":"2016/10/22", "hits": 402},
-            {"day":"2016/10/23", "hits": 915}
-          ],
-          dateParser = d3.time.format('%Y/%m/%d').parse,
-          yScale = d3.scale.linear()
-                      .domain([0, d3.max(data, (d) => {
-                        return d.hits;
-                      })])
-                      .range([height, 0]),
-          xScale = d3.time.scale()
-                      .domain(d3.extent(data, (d) => {
-                        const date = dateParser(d.day);
-                        return date;
-                      }))
-                      .range([0,  width]),
-          xAxis = d3.svg.axis()
-                    .scale(xScale)
-                    .orient('bottom')
-                    .ticks(d3.time.days, 1)
-                    .tickFormat(d3.time.format("%a")),
-          yAxis = d3.svg.axis()
-                    .scale(yScale)
-                    .orient('left'),
-          yGrid = d3.svg.axis()
-                    .scale(yScale)
-                    .orient('left')
-                    .tickSize(-width, 0, 0)
-                    .tickFormat(""),
-          xGrid = d3.svg.axis()
-                    .scale(xScale)
-                    .orient('bottom')
-                    .tickSize(-height, 0, 0)
-                    .tickFormat(""),
-          line = d3.svg.line()
-                    .x((d) => {
-                      const date = dateParser(d.day);
-                      return xScale(date);
-                    })
-                    .y((d) => {
-                      return yScale(d.hits);
-                    }),
-          cx = (d) => {
-            const date = dateParser(d.day);
-            return xScale(date);
-          },
-          cy = (d) => {
-            return yScale(d.hits);
-          },
-          circle = (data).map((d, i) => {
-            return(
-                <circle
-                  r={2}
-                  key={i}
-                  cx={cx(d)}
-                  cy={cy(d)}
-                />
-            )
-          });
-
-    return(
-      <div id="chart-container" style={{width: '100%'}}>
-        <svg
-          id={this.props.svgId}
-          height={this.props.svgHeight}
-          width={this.props.svgWidth}
-          viewBox="0 0 1920 600"
-          style={{width: '100%', minWidth: '250px', height: 'auto'}}
-          preserveAspectRatio="xMinYMin meet"
-        >
-          <g transform={transform}>
-            <Grid height={height} grid={yGrid} gridType="y"/>
-            <Axis height={height} width={width} axis={xAxis} axisType="x" />
-            <Axis height={height} axis={yAxis} axisType="y" />
-            <path className="path" d={line(data)} strokeLinecap="round"/>
-            {circle}
-          </g>
-        </svg>
+class SimpleLineChart extends Component {
+  render() {
+    const data = [
+          {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+          {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+          {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+          {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+          {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+          {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+          {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+    ];
+    return (
+      <div style={{width: '100%', height: '500'}}>
+        <ResponsiveContainer>
+          <LineChart width={600} height={300} data={data}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+           <XAxis dataKey="name"/>
+           <YAxis/>
+           <CartesianGrid strokeDasharray="3 3"/>
+           <Tooltip/>
+           <Legend />
+           <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+           <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     );
   }
-});
+}
 
-export default LineChart;
+export default SimpleLineChart;
