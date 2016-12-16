d618ff4ccd269d30ad0afedcdc0506363334a3be
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-12-16 09:48:52 -0500

    Editing the recharts import to fix the build

--- gulp/src/analytics/components/Charts/Bar/BarChart.js
+++ gulp/src/analytics/components/Charts/Bar/BarChart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Component} from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'Recharts';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 
 class SimpleBarChart extends Component {
   render() {
